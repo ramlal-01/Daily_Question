e9b193cad249eceb9447ca9cd947b05514224ac9
ramlal-01 <ramlal0801@gmail.com>
2025-08-14 16:27:35 +0530

    Create 1927-MaximumAscendingSubarraySum

---
+++ 1927-MaximumAscendingSubarraySum/1927-MaximumAscendingSubarraySum.java
@@ -0,0 +1,18 @@
+// Last updated: 14/8/2025, 4:27:37 pm
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int sum = nums[0], max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                sum += nums[i];  
+            } else {
+                max = Math.max(max, sum);  
+                sum = nums[i]; 
+            }
+        }
+        
+        max = Math.max(max, sum);  
+        return max;
+    }
+}
