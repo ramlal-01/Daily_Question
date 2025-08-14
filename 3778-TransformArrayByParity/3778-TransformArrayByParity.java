c3f8e5f8d1b8e013311e4133cdc539738c13685f
ramlal-01 <ramlal0801@gmail.com>
2025-08-14 16:25:09 +0530

    Create 3778-TransformArrayByParity

---
+++ 3778-TransformArrayByParity/3778-TransformArrayByParity.java
@@ -0,0 +1,13 @@
+// Last updated: 14/8/2025, 4:25:11 pm
+class Solution {
+    public int[] transformArray(int[] nums) {
+        for(int i=0; i<nums.length; i++){
+            if( nums[i]%2==0){
+                nums[i]=0;
+            }
+            else nums[i]=1;
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
