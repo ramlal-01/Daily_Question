39608b7fa9d159569647a383177288e79476ee4f
ramlal-01 <ramlal0801@gmail.com>
2025-08-16 12:13:17 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.java
@@ -0,0 +1,47 @@
+// Last updated: 16/8/2025, 12:13:18 pm
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int ind = -1 ;
+        int n = nums.length ;
+        for( int i= n-2 ; i>=0 ; i--){
+            if( nums[i]<nums[i+1]){
+                ind = i;
+                break ; 
+            }
+        }
+
+        if( ind==-1) {
+            reverse( nums , 0 , n-1); 
+            return ; 
+        }
+        for ( int i = n-1 ; i>ind ; i--){
+            if( nums[i]> nums[ind]){
+                swap( nums , i , ind );
+                break;
+            }
+        }
+
+        reverse( nums , ind +1, n-1);
+
+    }  
+ 
+    public static void reverse(int[] arr , int left ,int right) { 
+
+        while (left < right) {
+            // Swap elements
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
