b2cb342079b8c8542999cc8fdbc989b75e6e72d4
ramlal-01 <ramlal0801@gmail.com>
2025-08-20 23:23:42 +0530

    Create 863-AllNodesDistanceKinBinaryTree

---
+++ 863-AllNodesDistanceKinBinaryTree/863-AllNodesDistanceKinBinaryTree.java
@@ -0,0 +1,63 @@
+// Last updated: 20/8/2025, 11:23:42 pm
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+    class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        HashMap<TreeNode,TreeNode> parentmap=new HashMap<>();
+        findParent(parentmap,root);
+        Queue<TreeNode> q=new LinkedList<>();
+        HashSet<TreeNode> visited =new HashSet<>();
+        q.add(target);
+
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode curr=q.poll();
+                visited.add(curr);
+                if(k==0){
+                    res.add(curr.val);
+                }
+                if(parentmap.containsKey(curr) && !visited.contains(parentmap.get(curr))){
+                    q.add(parentmap.get(curr));
+                }
+                if(curr.left!=null && !visited.contains(curr.left)){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null && !visited.contains(curr.right)){
+                    q.add(curr.right);
+                }
+            }
+            k--;
+            if(k<0){
+                break;
+            }
+        }
+        return res;
+    }
+    static void findParent(HashMap<TreeNode,TreeNode> hm,TreeNode root){
+        if(root==null){
+            return;
+        }
+        if(root.left!=null){
+            hm.put(root.left,root);
+        }
+         if(root.right!=null){
+            hm.put(root.right,root);
+        }
+        findParent(hm,root.left);
+        findParent(hm,root.right);
+        return;
+
+    }
+}
