69a17d1c04f277a9ed7e2ecfc4fa309000a39894
ramlal-01 <ramlal0801@gmail.com>
2025-08-22 10:50:09 +0530

    Create 1302-DeepestLeavesSum

---
+++ 1302-DeepestLeavesSum/1302-DeepestLeavesSum.java
@@ -0,0 +1,48 @@
+// Last updated: 22/8/2025, 10:50:10 am
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        levelorder(root ,list);
+
+        // System.out.println( list[0]);
+        int sum = 0 ;
+        for(int i = 0 ; i<  list.get(list.size()-1 ).size() ; i++ ){
+            sum+= list.get(list.size()-1 ).get(i);
+        }
+        return sum ;
+    }
+
+    public void levelorder( TreeNode root , List<List<Integer>> list){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> arr = new ArrayList<>();
+
+            for( int i = 0 ; i<size ; i++){
+                TreeNode n = q.poll();
+                arr.add(n.val);
+                if( n.left!=null) q.add(n.left);
+                if( n.right!=null) q.add(n.right);
+            }
+
+            list.add(arr);
+        }
+    }
+}
