fafb56ce44c134308620c1ec125c118e6ceaa78f
ramlal-01 <ramlal0801@gmail.com>
2025-08-22 10:57:22 +0530

    File updated at 22/8/2025, 10:57:23 am

--- 1302-DeepestLeavesSum/1302-DeepestLeavesSum.java
+++ 1302-DeepestLeavesSum/1302-DeepestLeavesSum.java
@@ -1,4 +1,4 @@
-// Last updated: 22/8/2025, 10:50:10 am
+// Last updated: 22/8/2025, 10:57:23 am
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -17,27 +17,29 @@
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
         List<List<Integer>> list = new ArrayList<>();
-        levelorder(root ,list);
+        
+        int sum[] = {0} ;
+        levelorder(root ,list , sum);
 
         // System.out.println( list[0]);
-        int sum = 0 ;
-        for(int i = 0 ; i<  list.get(list.size()-1 ).size() ; i++ ){
-            sum+= list.get(list.size()-1 ).get(i);
-        }
-        return sum ;
+        // for(int i = 0 ; i<  list.get(list.size()-1 ).size() ; i++ ){
+        //     sum+= list.get(list.size()-1 ).get(i);
+        // }
+        return sum[0] ;
     }
 
-    public void levelorder( TreeNode root , List<List<Integer>> list){
+    public void levelorder( TreeNode root , List<List<Integer>> list , int[] sum){
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
 
         while(!q.isEmpty()){
             int size = q.size();
             List<Integer> arr = new ArrayList<>();
-
+            sum[0]=0;
             for( int i = 0 ; i<size ; i++){
                 TreeNode n = q.poll();
                 arr.add(n.val);
+                sum[0]+=n.val;
                 if( n.left!=null) q.add(n.left);
                 if( n.right!=null) q.add(n.right);
             }
