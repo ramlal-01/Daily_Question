9583a26b664d7062ddcb108b5fc653fb4ee245ea
ramlal-01 <ramlal0801@gmail.com>
2025-08-24 01:27:08 +0530

    Create 2415-ReverseOddLevelsofBinaryTree

---
+++ 2415-ReverseOddLevelsofBinaryTree/2415-ReverseOddLevelsofBinaryTree.java
@@ -0,0 +1,37 @@
+// Last updated: 24/8/2025, 1:27:07 am
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        dfs(root.left , root.right , 1 );
+        return root;
+    }
+
+    public void dfs(TreeNode left , TreeNode right , int level){
+        if( left==null || right==null){
+            return ;
+        }
+
+        if( level%2!=0){
+            int temp = left.val;
+            left.val = right.val;
+            right.val = temp ;
+        }
+
+        dfs(left.left , right.right , level+1);
+        dfs(left.right , right.left , level +1);
+    }
+}
