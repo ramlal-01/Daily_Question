cfd258a211b029803b8279bb6a8c9afe2c79270d
ramlal-01 <ramlal0801@gmail.com>
2025-09-05 11:47:02 +0530

    File updated at 5/9/2025, 11:47:03 am

--- 662-MaximumWidthofBinaryTree/662-MaximumWidthofBinaryTree.java
+++ 662-MaximumWidthofBinaryTree/662-MaximumWidthofBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 20/8/2025, 11:21:40 pm
+// Last updated: 5/9/2025, 11:47:03 am
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,47 +14,44 @@
  *     }
  * }
  */
- class Pair{
-            TreeNode node;
-            int num;
-            Pair(TreeNode _node,int _num){
-                num= _num;
-                node= _node;
-            }
-        }
+
+class Pair{
+    TreeNode node;
+    int ind ;
+
+    Pair( TreeNode r , int n){
+        node = r;
+        ind = n;
+    }
+}
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        int ans=0;
-        Queue<Pair> q=new LinkedList<>();
-        q.offer(new Pair(root,0));
-        while(!q.isEmpty()){
-            int size=q.size();
-            int min=q.peek().num;
-            int first=0;
-            int last=0;
-            for(int i=0;i<size;i++){
-                int cur_id=q.peek().num-min;
-                TreeNode node=q.peek().node;
-                q.poll();
-                if(i==0){
-                    first=cur_id;
-                }
-                if(i==size-1){
-                    last=cur_id;
-                }
-                if(node.left!=null){
-                    q.offer(new Pair(node.left,cur_id*2+1));
-                }
-                 if(node.right!=null){
-                    q.offer(new Pair(node.right,cur_id*2+2));
-                }
-            }
-            ans=Math.max(ans,last-first+1);
-        }
-        return ans;
 
+        if( root==null) return 0;
+
+        Queue<Pair> q = new LinkedList<>();
+        int width = 0;
+
+        q.add( new Pair(root,0));
+
+        while( !q.isEmpty()){
+            int size = q.size();
+            int first =0 , last =0 ;
+            int mmin = q.peek().ind ;
+
+            for (int i=0 ; i<size ; i++){
+                int curr = q.peek().ind - mmin ;
+                TreeNode cnode = q.peek().node;
+                q.poll();
+
+                if( i==0) first = curr;
+                if( i==size-1) last = curr;
+                if( cnode.left!=null) q.add( new Pair(cnode.left, 2*curr+1));
+                if( cnode.right!=null ) q.add( new Pair( cnode.right , 2*curr+2));
+            }
+            width = Math.max( width ,last - first +1) ;
+        }
+
+        return width ;
     }
 }
