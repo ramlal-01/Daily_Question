5ebc09777c49ea0f1e3727794c9e080a45f553dd
ramlal-01 <ramlal0801@gmail.com>
2025-09-15 23:39:29 +0530

    File updated at 15/9/2025, 11:39:29 pm

--- 802-FindEventualSafeStates/802-FindEventualSafeStates.java
+++ 802-FindEventualSafeStates/802-FindEventualSafeStates.java
@@ -1,38 +1,42 @@
-// Last updated: 14/9/2025, 5:40:02 pm
-class Solution {
-    public boolean dfs(int[][] graph , boolean[] visited , int node ,int[] path ,  int check[]  ){
-        visited[node] = true ;
-        path[node]= 1;
-        check[node]= 0;
-        for( int ele : graph[node]){
-            if( !visited[ele]){
-                if( dfs( graph , visited , ele , path , check)) return true;
-            }
-            
-            else if( path[ele]==1){
-                return true;
-            }
-        }
-        path[node]=0;
-        check[node]= 1;
-        return false;
-    }
-    public List<Integer> eventualSafeNodes(int[][] graph) { 
+// Last updated: 15/9/2025, 11:39:29 pm
+class Solution { 
+    public List<Integer> eventualSafeNodes(int[][] graph) {  
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int V = graph.length;
-        boolean[] visited = new boolean[V];
-        int[] path = new int[V];
-        int[] check = new int[V];
-        
+        for( int i =0 ; i<V ; i++){
+            adj.add( new ArrayList<>());
+        }    
+        int indegree[] = new int[V];
+         
         for( int i=0 ; i<V ; i++){
-            if( !visited[i]){
-                dfs( graph , visited , i , path , check);
+            for( int ele : graph[i]){
+                adj.get( ele).add(i);
+                indegree[i]++;
             }
         }
-
-        List<Integer> list = new ArrayList<>();
-        for( int i = 0 ; i<V  ;i++){
-            if( check[i]==1 ) list.add( i);
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0 ; i<V ; i++){
+            if( indegree[i]==0){
+                q.add( i);
+            }
         }
-        return list ;
+        ArrayList<Integer> topo = new ArrayList<>();
+       
+        while( !q.isEmpty()){
+            int node = q.peek();
+            q.poll();
+            topo.add(node);
+            
+            for( int ele : adj.get( node)){
+                indegree[ele]--;
+                
+                if( indegree[ele]==0){
+                    q.add( ele );
+                }
+            }
+        }
+        Collections.sort(topo);
+        return topo ;
     }
 }
