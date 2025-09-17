d1b98ae9ed6f849746824391bce129a91d94e380
ramlal-01 <ramlal0801@gmail.com>
2025-09-17 15:49:38 +0530

    File updated at 17/9/2025, 3:49:39 pm

--- 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.java
+++ 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.java
@@ -1,57 +1,54 @@
-// Last updated: 17/9/2025, 3:47:21 pm
-class Pair{
-    int dist ; 
-    int row ; 
-    int col ;
-    Pair( int d , int r , int c){
-        dist =d ;
-        row = r ;
-        col = c ;
+// Last updated: 17/9/2025, 3:49:39 pm
+class Pair {
+    int dist;
+    int row;
+    int col;
+    Pair(int d, int r, int c) {
+        dist = d;
+        row = r;
+        col = c;
     }
 }
+
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-// Your code here
-        int m = grid.length ;
-        int n = m ; 
-        int dist[][] = new int[m][n];
+        int m = grid.length;
+        int n = m;
 
+        // If start or end is blocked
         if (grid[0][0] == 1 || grid[m-1][n-1] == 1) return -1;
 
-        int[] source = {0,0};
-        int[] destination = {m-1,m-1};
-        for(int i=0 ; i<m ; i++){
-            for( int j =0 ; j<n ; j++){
-                dist[i][j] = Integer.MAX_VALUE ; 
-            }
+        int[][] dist = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
-        
-        Queue<Pair> q = new LinkedList<>();
 
-        q.add( new Pair( 1 , source[0], source[1]));
-        
-        dist[source[0]][source[1]]= 1 ;
-        
-        int delrow[] = {-1,-1, 0,1, 1,1,0,-1};
-        int delcol[] = { 0, 1,1,1, 0, -1 ,-1,-1 };
-            
-        while(!q.isEmpty()){
-            int dis = q.peek().dist ;
-            int r = q.peek().row ; 
-            int c = q.peek().col ;
-            q.poll();
-            
-            if( r==n-1 && c==n-1) return dis;
-            for( int k = 0 ; k<8 ; k++){
-                int nrow = r+ delrow[k];
-                int ncol = c+ delcol[k];
-                if( nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==0 && dis+1 < dist[nrow][ncol] ){
-                    dist[nrow][ncol] = dis+ 1;
-                    q.add( new Pair(dis+1 , nrow , ncol));
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(1, 0, 0)); // Start distance as 1 (not 0)
+        dist[0][0] = 1;
+
+        int[] delrow = {-1,-1, 0,1, 1,1,0,-1};
+        int[] delcol = { 0, 1,1,1, 0,-1,-1,-1};
+
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+            int dis = p.dist, r = p.row, c = p.col;
+
+            // If we reach destination
+            if (r == m-1 && c == n-1) return dis;
+
+            for (int k = 0; k < 8; k++) {
+                int nr = r + delrow[k];
+                int nc = c + delcol[k];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 0
+                        && dis + 1 < dist[nr][nc]) {
+                    dist[nr][nc] = dis + 1;
+                    q.add(new Pair(dis + 1, nr, nc));
                 }
             }
         }
-        
-        return dist[destination[0]][destination[1]]!= Integer.MAX_VALUE ? dist[destination[0]][destination[1]] : -1;
+
+        return -1;
     }
 }
