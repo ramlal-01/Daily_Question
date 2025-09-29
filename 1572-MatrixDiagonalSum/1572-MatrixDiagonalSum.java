2a1af660fd0473a5ad1032f64e52e1609c8553d3
ramlal-01 <ramlal0801@gmail.com>
2025-09-29 13:15:04 +0530

    File updated at 29/9/2025, 1:15:04 pm

--- 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
+++ 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
@@ -1,18 +1,13 @@
-// Last updated: 29/9/2025, 1:12:30 pm
+// Last updated: 29/9/2025, 1:15:04 pm
 class Solution {
     public int diagonalSum(int[][] mat) {
         int sum = 0 ;
+        int n = mat.length ; 
         for( int i =0 ; i<mat.length ; i++){
             sum += mat[i][i];
-        }
-
-        int n = mat.length ; 
-
-        for( int i =0 ; i< mat.length ; i++){
             if( i==( n-i-1)) continue ;
             sum+= mat[i][n-i-1];
         }
-
-        return sum ;
+        return sum   ;
     }
 }
