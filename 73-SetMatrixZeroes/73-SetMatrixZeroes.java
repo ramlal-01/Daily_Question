3efc6997b223bbe0b9313c97dca0ad54440497eb
ramlal-01 <ramlal0801@gmail.com>
2025-09-29 14:49:26 +0530

    File updated at 29/9/2025, 2:49:26 pm

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,42 +1,30 @@
-// Last updated: 16/8/2025, 6:04:44 pm
+// Last updated: 29/9/2025, 2:49:26 pm
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int col0 = 1 ; 
-        int m = matrix.length;
-        int n = matrix[0].length ;
-        for( int i=0; i<m ; i++ ){
-            for( int j= 0 ; j<n ; j++){
-                if( matrix[i][j]==0 ){
-                    matrix[i][0] = 0 ; 
-                
+        int m = matrix.length ;
+        int n= matrix[0].length ;
 
-                    if( j!=0 ){
-                        matrix[0][j]=0;
-                    }
-                    else col0 = 0 ;
+        boolean vis[][] = new boolean[m][n];
+
+        for( int i =0 ; i<m ; i++){
+            for( int j = 0 ; j<n ; j++){
+                if( matrix[i][j]==0){
+                    vis[i][j]=true;
                 }
             }
         }
 
-        for( int i= 1 ; i<m ; i++){
-            for( int j= 1; j<n ; j++){
-                if( matrix[i][j]!=0 ) {
-                    if( matrix[0][j]==0 || matrix[i][0]==0){
-                        matrix[i][j]=0;
+        for( int i = 0 ; i<m ; i++){
+            for( int j = 0 ; j< n ; j++){
+                if( vis[i][j]){
+                    for( int k = 0 ; k<m ; k++){
+                        matrix[k][j] = 0;
+                    }
+
+                    for( int k = 0 ; k<n ; k++){
+                        matrix[i][k] = 0 ;
                     }
                 }
-            }
-        }
-
-        if( matrix[0][0]==0){
-            for( int j=0 ; j<n ; j++){
-                matrix[0][j]=0;
-            }
-        }
-
-        if( col0==0){
-            for( int i=0 ; i<m ; i++){
-                matrix[i][0]=0;
             }
         }
     }
