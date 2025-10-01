2e1386bc687d08c7d7b40f60a7d475cee9bf9eac
ramlal-01 <ramlal0801@gmail.com>
2025-10-01 08:42:33 +0530

    File updated at 1/10/2025, 8:42:34 am

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
@@ -1,19 +1,38 @@
-// Last updated: 30/9/2025, 11:22:25 pm
+// Last updated: 1/10/2025, 8:42:34 am
 class Solution {
+    public boolean allZeroes( int[] map){
+        for( int ele : map){
+            if( ele!=0) return false ;
+        }
+        return true;
+    }
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
-        if (p.length() > s.length()) return res;
-        int[] a = new int[26];
-        int[] b = new int[26];
-        for (int i = 0; i < p.length(); i++) {
-            a[p.charAt(i) - 'a']++;
-            b[s.charAt(i) - 'a']++;
+        int n = s.length();
+
+        int map[] = new int[26];
+
+        for( int i =0  ; i< p.length() ; i++){
+            char ch = p.charAt(i);
+            map[ch-'a']++;
         }
-        if (java.util.Arrays.equals(a, b)) res.add(0);
-        for (int i = p.length(); i < s.length(); i++) {
-            b[s.charAt(i) - 'a']++;
-            b[s.charAt(i - p.length()) - 'a']--;
-            if (java.util.Arrays.equals(a, b)) res.add(i - p.length() + 1);
+
+        int i =0 , j =0 ; 
+
+        int k = p.length() ;
+
+        while( j< n ){
+            map[ s.charAt(j)-'a']--;
+
+            if( j-i+1 == k){
+                if( allZeroes(map)){
+                    res.add(i);
+                }
+
+                map[s.charAt(i)-'a']++;
+                i++;
+            }
+            j++;
         }
         return res;
     }
