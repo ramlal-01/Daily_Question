1709f7556200e8a6960adf84f630102c3eb339e7
ramlal-01 <ramlal0801@gmail.com>
2025-10-01 11:07:36 +0530

    File updated at 1/10/2025, 11:07:37 am

--- 135-Candy/135-Candy.java
+++ 135-Candy/135-Candy.java
@@ -1,4 +1,4 @@
-// Last updated: 1/10/2025, 10:49:18 am
+// Last updated: 1/10/2025, 11:07:37 am
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
@@ -12,23 +12,21 @@ class Solution {
                 arr[i] = 1 ;
             }
         }
-
-        int arr2[] = new int[n];
-
-        arr2[n-1] = 1 ;
-
+        int temp = arr[n-1];
+        arr[n-1] = 1;
         for( int i = n -2 ;i>=0 ; i--){
             if( ratings[i]> ratings[i+1]){
-                arr2[i] = arr2[i+1] + 1;
+                arr[i] = Math.max(arr[i] , arr[i+1] + 1);
             }
-            else{
-                arr2[i] = 1 ;
-            }
+            // else{
+            //     arr[i] = Math.max( arr[i]) ;
+            // }
         }
         int c= 0 ;
-        for( int i =0 ;i<n ; i++){
-            c+= Math.max( arr[i] , arr2[i]);
+        for( int i =0 ;i<n-1 ; i++){
+            c+= arr[i];
         }
+        c+= Math.max( arr[n-1], temp);
         return c;
     }
 } 
