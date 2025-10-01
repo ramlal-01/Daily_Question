345d7c6ed0a45fcbb758d19df68bce53343541c0
ramlal-01 <ramlal0801@gmail.com>
2025-10-01 20:53:57 +0530

    File updated at 1/10/2025, 8:53:58 pm

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.java
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.java
@@ -1,26 +1,25 @@
-// Last updated: 27/9/2025, 5:35:09 am
+// Last updated: 1/10/2025, 8:53:58 pm
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort( points , (a,b) -> Integer.compare( a[0] , b[0]));
+        Arrays.sort( points, (a,b) -> Integer.compare(a[0] , b[0] ) );
+        int prevStart = points[0][0];
+        int prevEnd = points[0][1];
+        int c = 1 ;
+        for( int i = 1 ; i< points.length ; i++){
+            int currStart = points[i][0];
+            int currEnd = points[i][1];
 
-        int shots = 1 ;
-        int pS = points[0][0];
-        int pE = points[0][1];
-
-        for( int i =1 ; i< points.length ; i++){
-            int cS = points[i][0];
-            int cE = points[i][1];
-
-            if( cS <= pE){
-                pS = Math.max( pS , cS);
-                pE = Math.min( pE , cE);
+            if( currStart <= prevEnd){
+                prevStart = Math.max( prevStart , currStart);
+                prevEnd = Math.min( prevEnd  , currEnd);
             }
-            else{
-                shots++;
-                pS = cS;
-                pE = cE;
+            else {
+                c++;
+                prevStart = currStart;
+                prevEnd = currEnd ;
+
             }
         }
-        return shots;
+        return c ;
     }
 }
