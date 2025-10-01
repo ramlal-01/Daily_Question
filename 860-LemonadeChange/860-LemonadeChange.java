374a965a68652d9fbefcde553e9ec6757e5ce03e
ramlal-01 <ramlal0801@gmail.com>
2025-10-01 23:56:32 +0530

    Create 860-LemonadeChange

---
+++ 860-LemonadeChange/860-LemonadeChange.java
@@ -0,0 +1,34 @@
+// Last updated: 1/10/2025, 11:56:32 pm
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int arr[] = new int[3] ;
+
+        for ( int i = 0 ; i< bills.length ; i++){
+
+            if( bills[i]==5){
+                arr[0]++;
+            }
+
+            else if( bills[i]==10){
+                arr[1]++;
+                if( arr[0]<1) return false ;
+                else arr[0]--;
+            }
+
+            else{
+                arr[2]++;
+
+                if( (arr[1]>=1 && arr[0]>=1) ){
+                    arr[0]--;
+                    arr[1]-- ;
+                }
+                else if ( arr[0]>=3 ){
+                    arr[0] -= 3 ;
+                }
+
+                else return false ;
+            }
+        }
+        return true ;
+    }
+}
