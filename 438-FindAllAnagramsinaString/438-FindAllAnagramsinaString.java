cf2b2f673a3c958e7931a7fc642e388f21c36308
ramlal-01 <ramlal0801@gmail.com>
2025-10-03 04:30:39 +0530

    File updated at 3/10/2025, 4:30:39 am

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
@@ -1,39 +1,33 @@
-// Last updated: 1/10/2025, 8:42:34 am
+// Last updated: 3/10/2025, 4:30:39 am
 class Solution {
-    public boolean allZeroes( int[] map){
-        for( int ele : map){
-            if( ele!=0) return false ;
-        }
-        return true;
-    }
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-        int n = s.length();
+        int map[]= new int[26];
+        int map2[] = new int[26];
 
-        int map[] = new int[26];
+        List<Integer> ans = new ArrayList<>();
+        if (s.length() < p.length()) return ans;
 
-        for( int i =0  ; i< p.length() ; i++){
-            char ch = p.charAt(i);
-            map[ch-'a']++;
+        for( int i = 0 ; i<p.length() ; i++){
+            map[ s.charAt(i)-'a']++;
+            map2[ p.charAt(i)-'a']++;
         }
 
-        int i =0 , j =0 ; 
+        int i = 0;
+        int j = p.length() ;
 
-        int k = p.length() ;
+        
 
-        while( j< n ){
-            map[ s.charAt(j)-'a']--;
+        while( j< s.length()){
+            if( Arrays.equals( map , map2) ) ans.add(i);
+            map[ s.charAt(i)-'a']--;
+            map[ s.charAt(j)-'a']++;
+             
 
-            if( j-i+1 == k){
-                if( allZeroes(map)){
-                    res.add(i);
-                }
-
-                map[s.charAt(i)-'a']++;
-                i++;
-            }
+            i++;
             j++;
         }
-        return res;
+        if (Arrays.equals(map, map2)) ans.add(i);
+
+        return ans ;
     }
 }
