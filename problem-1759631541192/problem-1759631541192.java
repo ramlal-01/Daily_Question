4a3f94ead5b349b02dcaf9ebc74437c8f75996b1
ramlal-01 <ramlal0801@gmail.com>
2025-10-05 08:02:24 +0530

    Create

---
+++ problem-1759631541192/problem-1759631541192.java
@@ -0,0 +1,11 @@
+// Last updated: 5/10/2025, 8:02:21 am
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum =0 ;
+        for( int i =0 ; i< nums.length ; i++){
+            if( i%2==0) sum+= nums[i];
+            else sum-= nums[i];
+        }
+        return sum;
+    }
+}
