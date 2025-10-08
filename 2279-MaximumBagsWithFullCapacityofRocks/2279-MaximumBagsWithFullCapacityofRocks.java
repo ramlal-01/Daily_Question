c8cd9da4c5fd6e70cb66188d801822c8ed646c0f
ramlal-01 <ramlal0801@gmail.com>
2025-10-08 08:40:33 +0530

    File updated at 8/10/2025, 8:40:34 am

--- 2279-MaximumBagsWithFullCapacityofRocks/2279-MaximumBagsWithFullCapacityofRocks.java
+++ 2279-MaximumBagsWithFullCapacityofRocks/2279-MaximumBagsWithFullCapacityofRocks.java
@@ -1,4 +1,4 @@
-// Last updated: 8/10/2025, 8:21:06 am
+// Last updated: 8/10/2025, 8:40:34 am
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
         int n = rocks.length ; 
@@ -8,33 +8,17 @@ class Solution {
         for( int i = 0 ; i<n ; i++){
             need[i] = capacity[i]-rocks[i];
         }
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for( int i =0 ; i<n ; i++){
-            map.put( i , need[i]);
-        }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>( (a,b)-> map.get(a) - map.get(b));
-        pq.addAll( map.keySet());
-
-        // System.out.println( map);
-        //  System.out.println( pq);
-
-        while( !pq.isEmpty()){
-            int ind = pq.poll();
-            int ele = need[ind];
-
-            if( extra >= ele){
-                need[ind] = 0;
-                extra -= ele;
+        Arrays.sort(need);
+        int c =0 ; 
+        for( int i = 0 ; i<n ; i++){
+            if( need[i] == 0) c++;
+            else if( extra >= need[i] ){
+                extra -= need[i];
+                c++;
             } else {
                 break;  
             }
-        }
-        int c =0 ;
-        for( int ele : need){
-            if( ele == 0) c++;
-        }
+        } 
         return c;
     }
 }
