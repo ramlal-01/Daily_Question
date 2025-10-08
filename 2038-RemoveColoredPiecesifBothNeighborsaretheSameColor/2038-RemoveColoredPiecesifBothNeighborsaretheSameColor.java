796aaab202d0bbbb49f22bfc395bd5e4aff1b57b
ramlal-01 <ramlal0801@gmail.com>
2025-10-08 07:49:42 +0530

    File updated at 8/10/2025, 7:49:43 am

--- 2038-RemoveColoredPiecesifBothNeighborsaretheSameColor/2038-RemoveColoredPiecesifBothNeighborsaretheSameColor.java
+++ 2038-RemoveColoredPiecesifBothNeighborsaretheSameColor/2038-RemoveColoredPiecesifBothNeighborsaretheSameColor.java
@@ -1,29 +1,25 @@
-// Last updated: 8/10/2025, 7:47:28 am
+// Last updated: 8/10/2025, 7:49:43 am
 class Solution {
     public boolean winnerOfGame(String colors) {
         int ca = 0 ; 
         int tota = 0 ;
+        int cb = 0 ;
+        int totb = 0 ;
+
         for( char s : colors.toCharArray()){
             if( s=='A'){
                 ca++; 
+                if( cb>=3) totb = totb + (cb-2) ;
+                cb=0 ;
             }
             else{
+                cb++; 
                 if( ca>=3) tota = tota+ (ca-2) ;
                 ca=0 ;
             } 
         }
         if( ca>=3) tota = tota + (ca-2) ;
-        int cb = 0 ;
-        int totb = 0 ;
-        for( char s : colors.toCharArray()){
-            if( s=='B'){
-                cb++; 
-            }
-            else{
-                if( cb>=3) totb = totb + (cb-2) ;
-                cb=0 ;
-            } 
-        }
+        
         if( cb>=3) totb = totb + (cb-2) ;
         // System.out.println( tota +" "+ totb);
         return tota>totb ;
