2b5de0da9cc2a745ffd829e96da47586ed70e3c0
ramlal-01 <ramlal0801@gmail.com>
2025-10-14 05:01:26 +0530

    File updated at 14/10/2025, 5:01:28 am

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,26 +1,29 @@
-// Last updated: 6/10/2025, 2:01:34 pm
+// Last updated: 14/10/2025, 5:01:28 am
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        boolean[] vis = new boolean[nums.length];
-        helper( nums , res , new ArrayList<>()  , vis);
+        int vis[] = new int[nums.length];
+        helper( nums , res , new ArrayList<>() , vis );
         return res ;
     }
 
-    public void helper( int[] nums , List<List<Integer>> res , List<Integer> path , boolean[] vis ){
+    public void helper( int[] nums , List<List<Integer>> res , List<Integer> path , int[] vis ){
         if( path.size()==nums.length){
             res.add( new ArrayList<>(path));
-            return ;
-        }
-        for( int i =0 ; i<nums.length ; i++ ){
-            if( vis[i]) continue ;
-
-            vis[i]= true;;
-            path.add( nums[i] );
-            helper( nums , res , path , vis );
-            path.remove( path.size()-1);
-            vis[i] = false ;
+            return;
         }
 
+        for( int i =0 ; i<nums.length ; i++){
+            if( vis[i]!=0){
+                continue ;
+            }
+                vis[i]=1 ;
+                path.add(nums[i]);
+                helper( nums , res , path, vis);
+                path.remove( path.size()-1);
+                vis[i]=0;
+            
+            
+        }
     }
 }
