1f8fe546866895db103129d7e3974e0ea20416c8
Ram Lal <ramlal0801@gmail.com>
2025-10-22 21:39:01 +0530

    File updated at 22/10/2025, 9:39:01 pm

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,10 +1,21 @@
-// Last updated: 4/10/2025, 11:29:32 pm
+// Last updated: 22/10/2025, 9:39:01 pm
 class Solution {
     public int uniquePaths(int m, int n) {
-        int vis[][] = new int[m][n];
-        for( int ele[] : vis) Arrays.fill( ele , -1);
+        int dp[][] = new int[m][n];
+        dp[0][0] = 1;
 
-        return helper( 0 , 0 , m , n , vis);
+        for( int i =0 ; i< m ; i++){
+            for( int j = 0 ; j<n ; j++){
+                int left = 0 ; int right = 0 ;
+                if( i==0 &j==0) dp[0][0]=1;
+                else{
+                    if( i>0) left = dp[i-1][j];
+                    if( j>0) right = dp[i][j-1];
+                    dp[i][j] = left+right;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 
     public int helper( int row ,int col , int m , int n , int[][] vis){
