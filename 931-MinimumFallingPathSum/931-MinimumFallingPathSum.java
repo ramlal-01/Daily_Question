24c492a0a19882216b337770152817015d7870df
Ram Lal <ramlal0801@gmail.com>
2025-10-23 14:02:59 +0530

    File updated at 23/10/2025, 2:02:59 pm

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
@@ -1,27 +1,34 @@
-// Last updated: 23/10/2025, 1:49:28 pm
+// Last updated: 23/10/2025, 2:02:59 pm
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length ;
         int dp[][] = new int[n][n];
         int mini = Integer.MAX_VALUE;
-        for( int ele[] : dp ) Arrays.fill( ele , -101);
-        for( int i =0 ; i<n ;i++){
-            
-            int x = helper( n-1 , i , matrix , dp );
-            mini = Math.min( mini , x );
+        for( int i=0 ; i<n ; i++) {
+            dp[0][i] = matrix[0][i];
+        }
+        for( int i =1 ; i<n ;i++){
+            for( int j = 0 ; j<n ; j++){
+
+                int up = dp[i-1][j];
+                int dialeft =0 ;
+
+                if( j-1>=0) dialeft = dp[i-1][j-1];
+                else dialeft+= 1e9;
+
+                int diaright =0 ;
+
+                if( j+1<n ) diaright = dp[i-1][j+1];
+                else diaright+= 1e9 ;
+
+                dp[i][j]=matrix[i][j] + Math.min(up,Math.min(dialeft, diaright));
+            }
+        }
+
+        for( int i = 0 ; i<n ; i++){
+            mini = Math.min( mini , dp[n-1][i]);
         }
         return mini;
     }
-    private int helper(int i, int j, int[][] grid, int[][] dp) {
-
-        if (i<0||j<0||i>=grid.length || j>=grid.length) return Integer.MAX_VALUE; 
-        if (i == 0 ) return grid[0][j];
-        if (dp[i][j] != -101) return dp[i][j];
-
-        int up = helper(i-1, j, grid, dp);
-        int dialeft = helper(i-1, j-1, grid, dp);
-        int diaright = helper( i-1, j+1 , grid , dp );
-
-        return dp[i][j]=grid[i][j] + Math.min(up,Math.min(dialeft, diaright));
-    }
+   
 }
