aae5fabf67540c3d2340a6acde709c312bd10336
Ram Lal <ramlal0801@gmail.com>
2025-10-23 14:04:53 +0530

    File updated at 23/10/2025, 2:04:53 pm

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
@@ -1,32 +1,35 @@
-// Last updated: 23/10/2025, 2:02:59 pm
+// Last updated: 23/10/2025, 2:04:53 pm
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length ;
-        int dp[][] = new int[n][n];
+        int dp[] = new int[n];
+
         int mini = Integer.MAX_VALUE;
         for( int i=0 ; i<n ; i++) {
-            dp[0][i] = matrix[0][i];
+            dp[i] = matrix[0][i];
         }
         for( int i =1 ; i<n ;i++){
+            int curr[] = new int[n];
             for( int j = 0 ; j<n ; j++){
 
-                int up = dp[i-1][j];
+                int up = dp[j];
                 int dialeft =0 ;
 
-                if( j-1>=0) dialeft = dp[i-1][j-1];
+                if( j-1>=0) dialeft = dp[j-1];
                 else dialeft+= 1e9;
 
                 int diaright =0 ;
 
-                if( j+1<n ) diaright = dp[i-1][j+1];
+                if( j+1<n ) diaright = dp[j+1];
                 else diaright+= 1e9 ;
 
-                dp[i][j]=matrix[i][j] + Math.min(up,Math.min(dialeft, diaright));
+                curr[j]=matrix[i][j] + Math.min(up,Math.min(dialeft, diaright));
             }
+            dp = curr;
         }
 
         for( int i = 0 ; i<n ; i++){
-            mini = Math.min( mini , dp[n-1][i]);
+            mini = Math.min( mini , dp[i]);
         }
         return mini;
     }
