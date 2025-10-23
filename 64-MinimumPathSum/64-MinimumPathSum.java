ae571ea0387b0713522ccc2ca95f4148c52c1e6d
Ram Lal <ramlal0801@gmail.com>
2025-10-23 11:42:05 +0530

    File updated at 23/10/2025, 11:42:06 am

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,11 +1,27 @@
-// Last updated: 22/10/2025, 10:51:33 pm
+// Last updated: 23/10/2025, 11:42:06 am
 class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return helper(m - 1, n - 1, grid, dp);
+        // for (int[] row : dp) Arrays.fill(row, -1);
+        // return helper(m - 1, n - 1, grid, dp);
+
+        for( int i = 0 ; i<m ;i++){
+            for( int j =0 ; j<n ;j++){
+                if( i==0 && j==0 ) dp[i][j]=grid[i][j];
+                else{
+                    int up = grid[i][j] , left = grid[i][j] ;
+                    if( i>0) up+= dp[i-1][j];
+                    else up+= 1e9;
+                    if( j>0) left+= dp[i][j-1];
+                    else left+= 1e9;
+
+                    dp[i][j] = Math.min( up, left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 
     private int helper(int i, int j, int[][] grid, int[][] dp) {
