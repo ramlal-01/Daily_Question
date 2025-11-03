afa5e6754a24c6e3d1f967c4575cdbee82ed8488
Ram Lal <ramlal0801@gmail.com>
2025-11-03 15:09:32 +0530

    File updated at 3/11/2025, 3:09:33 pm

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
@@ -1,21 +1,23 @@
-// Last updated: 3/11/2025, 2:51:29 pm
+// Last updated: 3/11/2025, 3:09:33 pm
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
 
-        int dp[][] = new int[m+1][n+1];
+        int dp[] = new int[n+1];
 
         for( int i =1 ; i<=m ; i++){
+            int temp[] = new int[n+1];
             for( int j = 1 ; j<=n ; j++){
                 if( text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[i][j] =1+ dp[i-1][j-1];
+                    temp[j] =1+ dp[j-1];
                 }
                 else{
-                    dp[i][j] = Math.max( dp[i-1][j] , dp[i][j-1]);
+                    temp[j] = Math.max( dp[j] , temp[j-1]);
                 }
             }
+            dp = temp;
         }
-        return dp[m][n];
+        return dp[n];
     }
 }
