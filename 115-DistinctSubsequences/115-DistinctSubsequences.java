504cc60b9a34dcf9abb69e7494e2afa0063ee458
Ram Lal <ramlal0801@gmail.com>
2025-11-11 12:56:24 +0530

    Create 115-DistinctSubsequences

---
+++ 115-DistinctSubsequences/115-DistinctSubsequences.java
@@ -0,0 +1,21 @@
+// Last updated: 11/11/2025, 12:56:28 pm
+class Solution {
+    public int numDistinct(String s, String t) {
+        int dp[][] = new int[s.length()+1][t.length()+1];
+        for( int el[] : dp ) Arrays.fill( el , -1 );
+
+        return helper(s,t, s.length()-1 , t.length()-1 , dp);
+    }
+
+    public int helper( String s, String t , int i , int j , int[][] dp){
+        if( j<0) return 1 ;
+        if( i<0) return 0 ;
+
+        if( dp[i][j]!=-1) return dp[i][j];
+
+        if( s.charAt(i)==t.charAt(j)){
+            return dp[i][j] = helper(s,t, i-1,j-1 ,dp) + helper(s,t,i-1,j , dp);
+        }
+        return dp[i][j] = helper(s,t,i-1,j , dp);
+    }
+}
