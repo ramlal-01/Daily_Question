94d4e0c4a8aac022b7d05ec27144f11e9f8cdace
Ram Lal <ramlal0801@gmail.com>
2025-11-11 13:02:30 +0530

    File updated at 11/11/2025, 1:02:33 pm

--- 115-DistinctSubsequences/115-DistinctSubsequences.java
+++ 115-DistinctSubsequences/115-DistinctSubsequences.java
@@ -1,21 +1,22 @@
-// Last updated: 11/11/2025, 12:56:28 pm
+// Last updated: 11/11/2025, 1:02:33 pm
 class Solution {
     public int numDistinct(String s, String t) {
         int dp[][] = new int[s.length()+1][t.length()+1];
         for( int el[] : dp ) Arrays.fill( el , -1 );
 
-        return helper(s,t, s.length()-1 , t.length()-1 , dp);
+        return helper(s,t, s.length() , t.length() , dp);
     }
 
     public int helper( String s, String t , int i , int j , int[][] dp){
-        if( j<0) return 1 ;
-        if( i<0) return 0 ;
+        if( j==0) return 1 ;
+        if( i==0) return 0 ;
 
         if( dp[i][j]!=-1) return dp[i][j];
 
-        if( s.charAt(i)==t.charAt(j)){
+        if( s.charAt(i-1)==t.charAt(j-1)){
             return dp[i][j] = helper(s,t, i-1,j-1 ,dp) + helper(s,t,i-1,j , dp);
         }
+
         return dp[i][j] = helper(s,t,i-1,j , dp);
     }
 }
