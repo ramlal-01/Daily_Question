4b731749f8e99b595263fc8728d5ab8a31d67ac1
Ram Lal <ramlal0801@gmail.com>
2025-11-25 16:59:45 +0530

    Create 1015-SmallestIntegerDivisiblebyK

---
+++ 1015-SmallestIntegerDivisiblebyK/1015-SmallestIntegerDivisiblebyK.java
@@ -0,0 +1,18 @@
+// Last updated: 25/11/2025, 4:59:44 pm
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+      
+        if( k%2 ==0 || k%5==0) return -1 ;
+
+        int rem = 0;
+
+        for (int len = 1; len <= k; len++) {
+            rem = (rem * 10 + 1) % k;   
+
+            if (rem == 0) return len;
+        }
+
+        return -1; 
+
+    }
+}
