45ec64b41d55c244dd0efa96460305e3e75ef608
Ram Lal <ramlal0801@gmail.com>
2025-12-11 19:41:37 +0530

    File updated at 11/12/2025, 7:41:37 pm

--- 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.java
+++ 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.java
@@ -1,18 +1,12 @@
-// Last updated: 11/12/2025, 7:35:00 pm
+// Last updated: 11/12/2025, 7:41:37 pm
 1class Solution {
 2    public boolean check(int[] nums) {
 3        int n = nums.length ;
 4        int c = 0 ;
 5        for( int i =0 ; i<n ; i++){
-6            int j;
-7            for(  j = i ; j<n+i-1 ; j++){
-8                // System.out.println( nums[j%n] +" "+ nums[ (j+1)%n ]);
-9                if( nums[j%n] > nums[ (j+1)%n ] ){
-10                    break;
-11                }
-12            }
-13            if( j == n+i-1 ) return true ;
-14        }
-15        return false ;
-16    }
-17}
+6            if( nums[i] > nums[(i+1)%n] ) c++;
+7            if( c>1 ) return false ;
+8        }
+9        return true ;
+10    }
+11}
