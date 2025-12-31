3cc4b9b15871e3435c25fb8580908d0705c06d6e
Ram Lal <ramlal0801@gmail.com>
2025-12-31 12:53:47 +0530

    File updated at 31/12/2025, 12:53:48 pm

--- 210-CourseScheduleII/210-CourseScheduleII.java
+++ 210-CourseScheduleII/210-CourseScheduleII.java
@@ -1,47 +1,47 @@
-// Last updated: 14/9/2025, 11:45:28 pm
-class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int V = numCourses;
-        for( int i =0 ; i<V ; i++){
-            adj.add( new ArrayList<>());
-        }
-        for( int i =0 ; i<prerequisites.length ; i++){
-            adj.get( prerequisites[i][1]).add(prerequisites[i][0]);
-        }
-        
-        int indegree[] = new int[V];
-         
-        for( int i=0 ; i<V ; i++){
-            for( int ele : adj.get( i)){
-                indegree[ele]++;
-            }
-        }
-        
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0 ; i<V ; i++){
-            if( indegree[i]==0){
-                q.add( i);
-            }
-        }
-        int topo[] = new int[V];
-        int c =0 ;
-        int i = 0;
-        while( !q.isEmpty()){
-            int node = q.peek();
-            q.poll();
-            topo[i++] = node ;
-            c++;
-            for( int ele : adj.get( node)){
-                indegree[ele]--;
-                
-                if( indegree[ele]==0){
-                    q.add( ele );
-                }
-            }
-        }
-
-        if( c<V ) return new int[0];
-        return topo ;
-    }
-}
+// Last updated: 31/12/2025, 12:53:48 pm
+1class Solution {
+2    public int[] findOrder(int numCourses, int[][] prerequisites) {
+3        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+4        int V = numCourses;
+5        for( int i =0 ; i<V ; i++){
+6            adj.add( new ArrayList<>());
+7        }
+8        for( int i =0 ; i<prerequisites.length ; i++){
+9            adj.get( prerequisites[i][1]).add(prerequisites[i][0]);
+10        }
+11        
+12        int indegree[] = new int[V];
+13         
+14        for( int i=0 ; i<V ; i++){
+15            for( int ele : adj.get( i)){
+16                indegree[ele]++;
+17            }
+18        }
+19        
+20        Queue<Integer> q = new LinkedList<>();
+21        for(int i = 0 ; i<V ; i++){
+22            if( indegree[i]==0){
+23                q.add( i);
+24            }
+25        }
+26        int topo[] = new int[V];
+27        int c =0 ;
+28        int i = 0;
+29        while( !q.isEmpty()){
+30            int node = q.peek();
+31            q.poll();
+32            topo[i++] = node ;
+33            c++;
+34            for( int ele : adj.get( node)){
+35                indegree[ele]--;
+36                
+37                if( indegree[ele]==0){
+38                    q.add( ele );
+39                }
+40            }
+41        }
+42
+43        if( c<V ) return new int[0];
+44        return topo ;
+45    }
+46}
