109fe3337906f8205470fced35ee18b934828aa4
Ram Lal <ramlal0801@gmail.com>
2025-12-31 12:27:22 +0530

    File updated at 31/12/2025, 12:27:24 pm

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,42 +1,42 @@
-// Last updated: 14/9/2025, 10:58:20 pm
-class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int V = numCourses;
-        for( int i =0 ; i<V ; i++){
-            adj.add( new ArrayList<>());
-        }
-        for( int i =0 ; i<prerequisites.length ; i++){
-            adj.get( prerequisites[i][0]).add(prerequisites[i][1]);
-        }
-        
-        int indegree[] = new int[V];
-         
-        for( int i=0 ; i<V ; i++){
-            for( int ele : adj.get( i)){
-                indegree[ele]++;
-            }
-        }
-        
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0 ; i<V ; i++){
-            if( indegree[i]==0){
-                q.add( i);
-            }
-        } 
-        int c =0;
-        while( !q.isEmpty()){
-            int node = q.peek();
-            q.poll(); 
-            c++;            
-            for( int ele : adj.get( node)){
-                indegree[ele]--;
-                
-                if( indegree[ele]==0){
-                    q.add( ele );
-                }
-            }
-        }
-        return c==V ;
-    }
-}
+// Last updated: 31/12/2025, 12:27:24 pm
+1class Solution {
+2    public boolean canFinish(int numCourses, int[][] prerequisites) {
+3        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+4        int V = numCourses;
+5        for( int i =0 ; i<V ; i++){
+6            adj.add( new ArrayList<>());
+7        }
+8        for( int i =0 ; i<prerequisites.length ; i++){
+9            adj.get( prerequisites[i][0]).add(prerequisites[i][1]);
+10        }
+11        
+12        int indegree[] = new int[V];
+13         
+14        for( int i=0 ; i<V ; i++){
+15            for( int ele : adj.get( i)){
+16                indegree[ele]++;
+17            }
+18        }
+19        
+20        Queue<Integer> q = new LinkedList<>();
+21        for(int i = 0 ; i<V ; i++){
+22            if( indegree[i]==0){
+23                q.add( i);
+24            }
+25        } 
+26        int c =0;
+27        while( !q.isEmpty()){
+28            int node = q.peek();
+29            q.poll(); 
+30            c++;            
+31            for( int ele : adj.get( node)){
+32                indegree[ele]--;
+33                
+34                if( indegree[ele]==0){
+35                    q.add( ele );
+36                }
+37            }
+38        }
+39        return c==V ;
+40    }
+41}
