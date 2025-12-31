6fd4b814133d6662d03d392461f4ed17e2abbfe9
Ram Lal <ramlal0801@gmail.com>
2025-12-31 12:30:24 +0530

    File updated at 31/12/2025, 12:30:26 pm

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,4 +1,4 @@
-// Last updated: 31/12/2025, 12:28:46 pm
+// Last updated: 31/12/2025, 12:30:26 pm
 1class Solution {
 2    public boolean canFinish(int numCourses, int[][] prerequisites) {
 3        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -6,38 +6,31 @@
 5        for( int i =0 ; i<V ; i++){
 6            adj.add( new ArrayList<>());
 7        }
-8        for( int i =0 ; i<prerequisites.length ; i++){
-9            adj.get( prerequisites[i][0]).add(prerequisites[i][1]);
-10        }
-11        
-12        int indegree[] = new int[V];
-13         
-14        for( int i=0 ; i<V ; i++){
-15            for( int ele : adj.get( i)){
-16                indegree[ele]++;
+8        int indegree[] = new int[V];
+9        for( int i =0 ; i<prerequisites.length ; i++){
+10            adj.get( prerequisites[i][0]).add(prerequisites[i][1]);
+11            indegree[ prerequisites[i][1]]++;
+12        }
+13        Queue<Integer> q = new ArrayDeque<>();
+14        for(int i = 0 ; i<V ; i++){
+15            if( indegree[i]==0){
+16                q.add( i);
 17            }
-18        }
-19        
-20        Queue<Integer> q = new ArrayDeque<>();
-21        for(int i = 0 ; i<V ; i++){
-22            if( indegree[i]==0){
-23                q.add( i);
-24            }
-25        } 
-26        int c =0;
-27        while( !q.isEmpty()){
-28            int node = q.peek();
-29            q.poll(); 
-30            c++;            
-31            for( int ele : adj.get( node)){
-32                indegree[ele]--;
-33                
-34                if( indegree[ele]==0){
-35                    q.add( ele );
-36                }
-37            }
-38        }
-39
-40        return c==V ;
-41    }
-42}
+18        } 
+19        int c =0;
+20        while( !q.isEmpty()){
+21            int node = q.peek();
+22            q.poll(); 
+23            c++;            
+24            for( int ele : adj.get( node)){
+25                indegree[ele]--;
+26                
+27                if( indegree[ele]==0){
+28                    q.add( ele );
+29                }
+30            }
+31        }
+32
+33        return c==V ;
+34    }
+35}
