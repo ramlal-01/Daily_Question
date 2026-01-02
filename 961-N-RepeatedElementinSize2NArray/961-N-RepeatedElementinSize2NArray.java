65ea0d032d925b911d77df106ce3ec850eda01c8
Ram Lal <ramlal0801@gmail.com>
2026-01-02 08:16:30 +0530

    File updated at 2/1/2026, 8:16:30 am

--- 961-N-RepeatedElementinSize2NArray/961-N-RepeatedElementinSize2NArray.java
+++ 961-N-RepeatedElementinSize2NArray/961-N-RepeatedElementinSize2NArray.java
@@ -1,19 +1,12 @@
-// Last updated: 2/1/2026, 8:15:08 am
+// Last updated: 2/1/2026, 8:16:30 am
 1class Solution {
 2    public int repeatedNTimes(int[] nums) {
-3        int n = nums.length/2 ;
-4        int max = 0 ;
-5        for( int ele : nums) { max = Math.max( max , ele ) ; }
-6
-7        int map[] = new int[max+1];
-8
-9        for( int ele : nums){
-10            map[ele]++;
-11        }
-12
-13        for( int i=0;i<max+1 ; i++ ){
-14            if( map[i]== n ) return i ;
-15        }
-16        return -1 ;
-17    }
-18}
+3        Set<Integer> s = new HashSet<>();
+4        for(int i:nums)
+5        {
+6            if(!s.add(i))
+7              return i;
+8        }
+9        return nums[nums.length-1];
+10    }
+11}
