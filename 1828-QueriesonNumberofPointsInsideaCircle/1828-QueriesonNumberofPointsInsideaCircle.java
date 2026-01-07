1698a28f24b514623dd157ce2e7ee8793234d63e
Ram Lal <ramlal0801@gmail.com>
2026-01-07 07:33:36 +0530

    File updated at 7/1/2026, 7:33:37 am

--- 1828-QueriesonNumberofPointsInsideaCircle/1828-QueriesonNumberofPointsInsideaCircle.java
+++ 1828-QueriesonNumberofPointsInsideaCircle/1828-QueriesonNumberofPointsInsideaCircle.java
@@ -1,4 +1,4 @@
-// Last updated: 7/1/2026, 7:31:35 am
+// Last updated: 7/1/2026, 7:33:37 am
 1class Solution {
 2    public int[] countPoints(int[][] points, int[][] queries) {
 3        int arr[] = new int[queries.length];
@@ -9,10 +9,10 @@
 8            int r  = ele[2] ;
 9            int c= 0 ;
 10            for( int curr[] : points ){
-11                int x2 = curr[0] ;
-12                int y2 = curr[1] ;
+11                int x2 = x1 - curr[0] ;
+12                int y2 = y1 - curr[1] ;
 13                
-14                if( (Math.pow(( x2-x1) , 2) + Math.pow((y2-y1) ,2)) <= Math.pow(r,2) ){
+14                if( (x2*x2 + y2*y2 ) <= r*r ){
 15                    c++;
 16                }
 17            }
