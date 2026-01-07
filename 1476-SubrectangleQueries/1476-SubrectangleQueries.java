7ab9e8d87f0d188cd9de371522bf94413823923b
Ram Lal <ramlal0801@gmail.com>
2026-01-07 07:45:00 +0530

    File updated at 7/1/2026, 7:45:01 am

--- 1476-SubrectangleQueries/1476-SubrectangleQueries.java
+++ 1476-SubrectangleQueries/1476-SubrectangleQueries.java
@@ -1,29 +1,34 @@
-// Last updated: 7/1/2026, 7:42:08 am
+// Last updated: 7/1/2026, 7:45:01 am
 1class SubrectangleQueries {
 2
 3    int[][] rect;
-4
-5    public SubrectangleQueries(int[][] rectangle) {
-6        rect = rectangle;
-7    }
-8
-9    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-10        for (int i = row1; i <= row2; i++) {
-11            for (int j = col1; j <= col2; j++) {
-12                rect[i][j] = newValue;
-13            }
-14        }
-15    }
-16
-17    public int getValue(int row, int col) {
-18        return rect[row][col];
-19    }
-20}
-21
-22
-23/**
-24 * Your SubrectangleQueries object will be instantiated and called as such:
-25 * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
-26 * obj.updateSubrectangle(row1,col1,row2,col2,newValue);
-27 * int param_2 = obj.getValue(row,col);
-28 */
+4    List<int[]> updates;
+5
+6    public SubrectangleQueries(int[][] rectangle) {
+7        rect = rectangle;
+8        updates = new ArrayList<>();
+9    }
+10
+11    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+12        updates.add(new int[]{row1, col1, row2, col2, newValue});
+13    }
+14
+15    public int getValue(int row, int col) {
+16        for (int i = updates.size() - 1; i >= 0; i--) {
+17            int[] u = updates.get(i);
+18            if (row >= u[0] && row <= u[2] && col >= u[1] && col <= u[3]) {
+19                return u[4];
+20            }
+21        }
+22        return rect[row][col];
+23    }
+24}
+25
+26
+27
+28/**
+29 * Your SubrectangleQueries object will be instantiated and called as such:
+30 * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
+31 * obj.updateSubrectangle(row1,col1,row2,col2,newValue);
+32 * int param_2 = obj.getValue(row,col);
+33 */
