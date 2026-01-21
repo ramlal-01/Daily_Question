26f98e0cebafb55b84825a09b2bdc2a724e7fe04
Ram Lal <ramlal0801@gmail.com>
2026-01-21 14:46:24 +0530

    File updated at 21/1/2026, 2:46:23 pm

--- 2696-MinimumStringLengthAfterRemovingSubstrings/2696-MinimumStringLengthAfterRemovingSubstrings.java
+++ 2696-MinimumStringLengthAfterRemovingSubstrings/2696-MinimumStringLengthAfterRemovingSubstrings.java
@@ -1,24 +1,22 @@
-// Last updated: 21/1/2026, 2:30:29 pm
+// Last updated: 21/1/2026, 2:46:23 pm
 1class Solution {
 2    public int minLength(String s) {
-3        Stack<Character> st = new Stack<>() ;
+3        StringBuilder sb = new StringBuilder();
 4
-5        int n = s.length();
-6
-7        for( int i =0 ; i<n ; i++ ){
-8            if( !st.isEmpty() && st.peek()=='A' && s.charAt(i)=='B' ){
-9                st.pop();
-10                
-11            }
-12            else if( !st.isEmpty() && st.peek()=='C' && s.charAt(i)=='D' ){
-13                st.pop();
-14            }
-15            else st.push( s.charAt(i) );
-16            
+5        for (char ch : s.toCharArray()) {
+6            sb.append(ch);
+7
+8            int len = sb.length();
+9            if (len >= 2) {
+10                char a = sb.charAt(len - 2);
+11                char b = sb.charAt(len - 1);
+12
+13                if ((a == 'A' && b == 'B') || (a == 'C' && b == 'D')) {
+14                    sb.delete(len - 2, len);
+15                }
+16            }
 17        }
-18
-19       
-20
-21        return st.size() ;
-22    }
-23}
+18        return sb.length();
+19    }
+20}
+21
