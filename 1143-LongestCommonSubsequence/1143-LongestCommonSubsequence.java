6ddcbdbb62192fcb48e58092a830322e41f3aad6
Ram Lal <ramlal0801@gmail.com>
2026-01-21 15:34:20 +0530

    File updated at 21/1/2026, 3:34:20 pm

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,4 +1,4 @@
-// Last updated: 21/1/2026, 3:12:34 pm
+// Last updated: 21/1/2026, 3:34:20 pm
 1class Solution {
 2    public int longestCommonSubsequence(String text1, String text2) {
 3        int n = text1.length() ;
@@ -6,25 +6,22 @@
 5
 6        int dp[][] = new int[n+1][m+1] ;
 7
-8        for( int i =0 ; i<m ; i++){
-9            dp[0][i] = 0 ;
-10        }
-11
-12        for( int i =0 ;i<n ; i++){
-13            dp[i][0] = 0 ;
-14        }
-15
-16        for( int i =1 ; i<=n ; i++){
-17            for( int j = 1 ; j<=m ; j++){
-18                if( text1.charAt(i-1)==( text2.charAt(j-1) ) ){
-19                    dp[i][j] = 1+dp[i-1][j-1];
-20                }
-21                else {
-22                    dp[i][j] = Math.max( dp[i-1][j] , dp[i][j-1] );
-23                }
-24
-25            }
-26        }
-27        return dp[n][m] ;
-28    }
-29}
+8        for( int ele[] : dp ) Arrays.fill( ele , -1 );
+9
+10
+11        return helper( text1, text2 , n-1 , m-1 , dp );
+12    }
+13
+14    public int helper( String s1 , String s2 , int n , int m  , int[][] dp ){
+15        if( n<0 || m<0 ){
+16            return 0;
+17        }
+18
+19        if( dp[n][m]!=-1 ) return dp[n][m] ;
+20
+21        if( s1.charAt(n) == s2.charAt(m) ){
+22            return dp[n][m] = 1+ helper( s1 , s2 , n-1 , m-1 , dp);
+23        }
+24        return dp[n][m] = Math.max( helper( s1,s2,n-1,m , dp) , helper( s1,s2 , n , m-1 , dp) );
+25    }
+26}
