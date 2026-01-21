845e47410bca4a08b965f8de2ee196fcf8a94d6c
Ram Lal <ramlal0801@gmail.com>
2026-01-21 14:18:19 +0530

    File updated at 21/1/2026, 2:18:18 pm

--- 1544-MakeTheStringGreat/1544-MakeTheStringGreat.java
+++ 1544-MakeTheStringGreat/1544-MakeTheStringGreat.java
@@ -1,21 +1,26 @@
-// Last updated: 6/10/2025, 6:13:55 am
-class Solution {
-    public String makeGood(String s) { 
-        int n = s.length();
-
-        Deque<Character> st = new ArrayDeque<>();
-
-        for( int i =0 ; i<n ; i++){
-            if( !st.isEmpty() && Math.abs(st.peek()- s.charAt(i) )==32){
-                st.pop();
-            }
-            else{
-                st.push( s.charAt(i));
-            }
-            
-        }
-        StringBuilder sb = new StringBuilder();
-        while (!st.isEmpty()) sb.append(st.pop());
-        return sb.reverse().toString();
-    }
-}
+// Last updated: 21/1/2026, 2:18:18 pm
+1class Solution {
+2    public String makeGood(String s) {
+3        Stack<Character> st = new Stack<>() ;
+4
+5        int n = s.length();
+6
+7        for( int i =0 ; i<n ; i++ ){
+8            if( !st.isEmpty() && Math.abs(st.peek()-s.charAt(i))==32 ){
+9                st.pop();
+10                
+11            }
+12            else st.push( s.charAt(i) );
+13            
+14        }
+15
+16        StringBuilder sb = new StringBuilder();
+17        while( !st.isEmpty() ){
+18            sb.append(st.peek() );
+19            st.pop();
+20        }
+21        sb.reverse();
+22
+23        return sb.toString();
+24    }
+25}
