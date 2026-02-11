a1f6430b88c8f9643bf77fe7a63bdd13953f17e7
Ram Lal <ramlal0801@gmail.com>
2026-02-11 14:38:06 +0530

    File updated at 11/2/2026, 2:38:07 pm

--- 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
@@ -1,35 +1,33 @@
-// Last updated: 21/8/2025, 7:30:13 am
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-         if(root == null) return list ; 
-
-        Stack<TreeNode> st = new Stack<TreeNode>();
-        st.push(root);
-
-        while( !st.isEmpty()){
-            TreeNode n = st.pop();
-            
-            list.add(n.val);
-            
-            if(n.right!=null) st.push(n.right);
-            if(n.left!=null) st.push(n.left);
-        }
-        return list;
-    }
-}
+// Last updated: 11/2/2026, 2:38:07 pm
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<Integer> preorderTraversal(TreeNode root) {
+18        List<Integer> list = new ArrayList<>() ;
+19        helper( root , list );
+20        return list ;
+21    }
+22
+23    public void helper( TreeNode root , List<Integer> list ){
+24        if( root==null ){
+25            return ;
+26        }
+27
+28        list.add( root.val);
+29        helper( root.left , list );
+30        helper( root.right , list ) ;
+31    }
+32}
