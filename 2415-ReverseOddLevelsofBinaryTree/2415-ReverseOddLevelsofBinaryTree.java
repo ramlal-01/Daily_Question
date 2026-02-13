543bf7a5219fc6c2bc18d882f11022a0bd2a0590
Ram Lal <ramlal0801@gmail.com>
2026-02-13 22:38:27 +0530

    File updated at 13/2/2026, 10:38:28 pm

--- 2415-ReverseOddLevelsofBinaryTree/2415-ReverseOddLevelsofBinaryTree.java
+++ 2415-ReverseOddLevelsofBinaryTree/2415-ReverseOddLevelsofBinaryTree.java
@@ -1,39 +1,39 @@
-// Last updated: 24/8/2025, 1:27:53 am
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode reverseOddLevels(TreeNode root) {
-
-        if(root == null)
-        return root;
-        helper(root.left, root.right, 1);
-        return root;
-        
-    }
-
-    void helper(TreeNode left, TreeNode right, int l){
-        if(left == null || right == null) return;
-
-        if(l%2 == 1){
-            int temp = left.val;
-            left.val =right.val;
-            right.val = temp;
-        }
-
-        helper(left.left, right.right, l+1);
-        helper(left.right, right.left, l+1);
-    }
-}
+// Last updated: 13/2/2026, 10:38:28 pm
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode reverseOddLevels(TreeNode root) {
+18        if(root == null)
+19        return root;
+20        dfs(root.left , root.right , 1 );
+21        return root;
+22    }
+23
+24    public void dfs(TreeNode left , TreeNode right , int level){
+25        if( left==null || right==null){
+26            return ;
+27        }
+28
+29        if( level%2!=0){
+30            int temp = left.val;
+31            left.val = right.val;
+32            right.val = temp ;
+33        }
+34
+35        dfs(left.left , right.right , level+1);
+36        dfs(left.right , right.left , level +1);
+37    }
+38}
