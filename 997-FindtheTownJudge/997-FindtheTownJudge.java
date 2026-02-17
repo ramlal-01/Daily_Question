722ad01aa40e46684e0b21fcf16f7320a624cab5
Ram Lal <ramlal0801@gmail.com>
2026-02-17 20:49:44 +0530

    File updated at 17/2/2026, 8:49:46 pm

--- 997-FindtheTownJudge/997-FindtheTownJudge.java
+++ 997-FindtheTownJudge/997-FindtheTownJudge.java
@@ -1,16 +1,16 @@
-// Last updated: 14/10/2025, 5:20:50 am
-class Solution {
-    public int findJudge(int n, int[][] trust) {
-        int arr[] = new int[n+1];
-
-        for( int i = 0 ; i<trust.length ; i++){
-            arr[ trust[i][0]]--;
-            arr[ trust[i][1]]++;
-        }
-
-        for( int i = 1 ; i<arr.length ; i++){
-            if( arr[i] == n-1) return i;
-        }
-        return -1 ;
-    }
-}
+// Last updated: 17/2/2026, 8:49:46 pm
+1class Solution {
+2    public int findJudge(int n, int[][] trust) {
+3        int arr[] = new int[n+1];
+4
+5        for( int i = 0 ; i<trust.length ; i++){
+6            arr[ trust[i][0]]--;
+7            arr[ trust[i][1]]++;
+8        }
+9
+10        for( int i = 1 ; i<arr.length ; i++){
+11            if( arr[i] == n-1) return i;
+12        }
+13        return -1 ;
+14    }
+15}
