574ce572f67b8ebbf3d552dac52d8d2ffc2f9273
Ram Lal <ramlal0801@gmail.com>
2026-02-18 15:40:02 +0530

    File updated at 18/2/2026, 3:40:05 pm

--- 547-NumberofProvinces/547-NumberofProvinces.java
+++ 547-NumberofProvinces/547-NumberofProvinces.java
@@ -1,41 +1,28 @@
-// Last updated: 18/2/2026, 3:35:12 pm
+// Last updated: 18/2/2026, 3:40:05 pm
 1class Solution {
 2    public int findCircleNum(int[][] isConnected) {
-3        int n = isConnected.length ;
-4        List<List<Integer>> adj = new ArrayList<>();
-5        for( int i =0 ; i<n ; i++ ){
-6            adj.add( new ArrayList<>());
-7        }
-8
-9        for( int i =0 ; i<n ; i++ ){
-10            for( int j = 0 ; j< isConnected[i].length ; j++ ){
-11                if( isConnected[i][j]==1 ){
-12                    adj.get(i).add(j);
-13                    adj.get(j).add(i);
-14                }
-15            }
-16        }
-17
-18        boolean vis[] = new boolean[n] ;
-19        int c = 0 ;
-20        for( int i =0 ; i<n ; i++ ){
-21            if( !vis[i] ){
-22                dfs( adj , i , vis );
-23                c++;
-24            }
-25        }
-26        return c ;
-27    }
-28
-29    public void dfs( List<List<Integer>> adj , int node , boolean[] vis ){
-30        vis[node] = true ;
-31
-32        for( int ele : adj.get(node) ){
-33            if( !vis[ele]){
-34                dfs( adj , ele , vis );
-35            }
-36        }
-37    }
-38
-39
-40}
+3        int n = isConnected.length ; 
+4
+5        boolean vis[] = new boolean[n] ;
+6        int c = 0 ;
+7        for( int i =0 ; i<n ; i++ ){
+8            if( !vis[i] ){
+9                dfs( isConnected , i , vis );
+10                c++;
+11            }
+12        }
+13        return c ;
+14    }
+15
+16    public void dfs( int[][] isConnected , int node , boolean[] vis ){
+17        vis[node] = true ;
+18
+19        for( int ele = 0 ; ele <isConnected[node].length ; ele++){
+20            if( !vis[ele] && isConnected[node][ele]==1 ){
+21                dfs( isConnected , ele , vis );
+22            }
+23        }
+24    }
+25
+26
+27}
