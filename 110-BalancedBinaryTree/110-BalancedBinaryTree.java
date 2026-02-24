ee3215e867e96f754db753f1173d9cef2e46da31
Ram Lal <ramlal0801@gmail.com>
2026-02-24 15:22:45 +0530

    File updated at 24/2/2026, 3:22:44 pm

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.java
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 12/2/2026, 11:11:24 am
+// Last updated: 24/2/2026, 3:22:44 pm
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -16,22 +16,22 @@
 15 */
 16class Solution {
 17    public boolean isBalanced(TreeNode root) {
-18        return helper(root)!=-1 ;
-19    }
-20
-21    public int helper( TreeNode root ){
-22        if( root==null ){
-23            return 0 ;
-24        }
-25
-26        int left  = helper(root.left );
-27        if( left==-1 ) return -1 ;
+18        if( helper(root)==-1 ) return false ;
+19        return true;
+20    }
+21
+22    public int helper( TreeNode root ){
+23        if( root==null ){
+24            return 0 ;
+25        }
+26        int left = helper(root.left) ;
+27        if( left==-1) return -1 ;
 28
 29        int right = helper( root.right);
 30        if( right==-1 ) return -1 ;
 31
-32        if( Math.abs(right-left)>1 ) return -1 ;
+32        if( Math.abs( left-right )>1 ) return -1 ;
 33
-34        return 1+ Math.max( left , right );
+34        return 1+Math.max( left , right );
 35    }
 36}
