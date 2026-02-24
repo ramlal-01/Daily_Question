a3cc833aaf18fa21636d7b55dc410d8145c6e2ae
Ram Lal <ramlal0801@gmail.com>
2026-02-24 15:21:30 +0530

    File updated at 24/2/2026, 3:21:30 pm

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
@@ -1,41 +1,36 @@
-// Last updated: 21/8/2025, 10:28:20 am
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        if( root == null){
-            return 0;
-        }
-        int max[] = {0} ;
-
-        diameter( root , max );
-
-        return max[0]; 
-    }
-
-    public int diameter( TreeNode root , int max[]){
-        if( root == null){
-            return 0 ;
-        }
-
-        int lh = diameter( root.left , max);
-        int rh = diameter( root.right , max);
-
-        max[0] = Math.max( max[0] , lh + rh );
-
-        return 1 + Math.max(lh , rh );
-    }
-}
+// Last updated: 24/2/2026, 3:21:30 pm
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    int max = 0 ;
+18    public int diameterOfBinaryTree(TreeNode root) {
+19        helper( root );
+20        return max ;
+21    }
+22
+23    public int helper( TreeNode root ){
+24        if( root == null ){
+25            return 0 ;
+26        }
+27
+28        int left = helper( root.left) ;
+29        int right = helper( root.right );
+30
+31        max = Math.max( max , left+right );
+32
+33        return 1+Math.max(left,right);
+34    }
+35}
