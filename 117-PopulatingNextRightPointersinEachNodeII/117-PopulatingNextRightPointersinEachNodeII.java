115617edf73338fe2172fa393b8442bfbd767aeb
Ram Lal <ramlal0801@gmail.com>
2026-02-28 01:17:13 +0530

    File updated at 28/2/2026, 1:17:14 am

--- 117-PopulatingNextRightPointersinEachNodeII/117-PopulatingNextRightPointersinEachNodeII.java
+++ 117-PopulatingNextRightPointersinEachNodeII/117-PopulatingNextRightPointersinEachNodeII.java
@@ -1,49 +1,49 @@
-// Last updated: 24/8/2025, 12:05:45 am
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
- 
-class Solution {
-    public Node connect(Node root) {
-        
-        if (root == null) return null;
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-
-            for( int i = 0 ; i<size ; i++){
-                Node n = q.poll();
-
-                if( n.left!=null) q.add(n.left);
-                if( n.right!=null) q.add(n.right);
-
-                if (i < size - 1) {
-                    n.next = q.peek();  
-                }
-            }
-        }
-        return root;
-    }
-}
+// Last updated: 28/2/2026, 1:17:14 am
+1/*
+2// Definition for a Node.
+3class Node {
+4    public int val;
+5    public Node left;
+6    public Node right;
+7    public Node next;
+8
+9    public Node() {}
+10    
+11    public Node(int _val) {
+12        val = _val;
+13    }
+14
+15    public Node(int _val, Node _left, Node _right, Node _next) {
+16        val = _val;
+17        left = _left;
+18        right = _right;
+19        next = _next;
+20    }
+21};
+22*/
+23
+24 
+25class Solution {
+26    public Node connect(Node root) {
+27        
+28        if (root == null) return null;
+29        Queue<Node> q = new LinkedList<>();
+30        q.add(root);
+31
+32        while(!q.isEmpty()){
+33            int size = q.size();
+34
+35            for( int i = 0 ; i<size ; i++){
+36                Node n = q.poll();
+37
+38                if( n.left!=null) q.add(n.left);
+39                if( n.right!=null) q.add(n.right);
+40
+41                if (i < size - 1) {
+42                    n.next = q.peek();  
+43                }
+44            }
+45        }
+46        return root;
+47    }
+48}
