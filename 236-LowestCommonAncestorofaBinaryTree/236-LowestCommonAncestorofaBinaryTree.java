6a6ae4da1689f90fbf25540416c882826bd99fe5
Ram Lal <ramlal0801@gmail.com>
2026-03-02 14:30:24 +0530

    File updated at 2/3/2026, 2:30:26 pm

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
@@ -1,26 +1,26 @@
-// Last updated: 21/8/2025, 2:55:54 pm
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if( root == null || root == p || root == q){
-            return root ;
-        }
-
-        TreeNode left = lowestCommonAncestor(root.left , p , q);
-        TreeNode right = lowestCommonAncestor( root.right , p , q);
-
-        if( left == null) return right ;
-        else if( right == null ) return left ;
-        else {
-            return root ;
-        }
-    }
-}
+// Last updated: 2/3/2026, 2:30:26 pm
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode(int x) { val = x; }
+8 * }
+9 */
+10class Solution { 
+11    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) { 
+12        if( root==null || root==p || root==q) {
+13            return root ;
+14        }
+15
+16        TreeNode left = lowestCommonAncestor(root.left , p , q);
+17        TreeNode right = lowestCommonAncestor(root.right , p , q);
+18
+19        if( left == null ){
+20            return right ;
+21        }
+22        if( right == null ) return left ;
+23        return root ;
+24}
+25}
