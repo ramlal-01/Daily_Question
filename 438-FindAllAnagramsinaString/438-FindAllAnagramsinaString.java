adda1259d59e1dde1c3d34fb3ac3c0789596ab11
Ram Lal <ramlal0801@gmail.com>
2026-03-09 16:31:20 +0530

    File updated at 9/3/2026, 4:31:20 pm

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
@@ -1,33 +1,26 @@
-// Last updated: 3/10/2025, 4:30:39 am
-class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        int map[]= new int[26];
-        int map2[] = new int[26];
-
-        List<Integer> ans = new ArrayList<>();
-        if (s.length() < p.length()) return ans;
-
-        for( int i = 0 ; i<p.length() ; i++){
-            map[ s.charAt(i)-'a']++;
-            map2[ p.charAt(i)-'a']++;
-        }
-
-        int i = 0;
-        int j = p.length() ;
-
-        
-
-        while( j< s.length()){
-            if( Arrays.equals( map , map2) ) ans.add(i);
-            map[ s.charAt(i)-'a']--;
-            map[ s.charAt(j)-'a']++;
-             
-
-            i++;
-            j++;
-        }
-        if (Arrays.equals(map, map2)) ans.add(i);
-
-        return ans ;
-    }
-}
+// Last updated: 9/3/2026, 4:31:20 pm
+1class Solution {
+2    public List<Integer> findAnagrams(String s, String p) {
+3        int map[]= new int[26];
+4        int map2[] = new int[26];
+5        List<Integer> ans = new ArrayList<>();
+6        if (s.length() < p.length()) return ans;
+7        for( int i = 0 ; i<p.length() ; i++){
+8            map[ s.charAt(i)-'a']++;
+9            map2[ p.charAt(i)-'a']++;
+10        }
+11        int i = 0;
+12        int j = p.length() ;
+13        while( j< s.length()){
+14            if( Arrays.equals( map , map2) ) ans.add(i);
+15            map[ s.charAt(i)-'a']--;
+16            map[ s.charAt(j)-'a']++;
+17             
+18
+19            i++;
+20            j++;
+21        }
+22        if (Arrays.equals(map, map2)) ans.add(i);
+23        return ans ;
+24    }
+25}
