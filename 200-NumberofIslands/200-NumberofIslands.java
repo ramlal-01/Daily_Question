e1fab76b5df24600c43e40247fc78aeaf9e216a2
Ram Lal <ramlal0801@gmail.com>
2026-03-10 11:07:29 +0530

    File updated at 10/3/2026, 11:07:30 am

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,39 +1,41 @@
-// Last updated: 30/12/2025, 11:49:46 am
+// Last updated: 10/3/2026, 11:07:30 am
 1class Solution {
 2    public int numIslands(char[][] grid) {
-3        int n = grid.length ;
-4        int m = grid[0].length ;
-5        boolean vis[][] = new boolean[n][m];
-6        int c= 0 ;
-7        for( int i = 0 ; i<n ; i++){
-8            for( int j = 0 ; j<m ;j++){
-9                if( grid[i][j]=='1' && !vis[i][j] ){
-10                    c++;
-11                    dfs( grid , vis , i , j );
-12                }
-13            }
-14        }
-15        return c ;
-16    }
+3        int m = grid.length ;
+4        int n = grid[0].length ;
+5
+6        boolean vis[][] = new boolean[m][n] ;
+7
+8        int cnt = 0 ;
+9        for( int i =0 ; i<m ; i++ ){
+10            for( int j= 0 ; j<n ; j++ ){
+11                if(grid[i][j]=='1' && !vis[i][j]){
+12                    dfs( grid , i , j , vis) ;
+13                    cnt++ ;
+14                }
+15            }
+16        }
 17
-18    public void dfs( char[][] grid ,boolean vis[][] , int row , int col  ){
-19        if( vis[row][col]) return ;
+18        return cnt ;
+19    }
 20
-21        vis[row][col] = true ;
-22        
-23        int drow[] = {-1,0,1,0};
-24        int dcol[] = {0,1,0,-1};
-25
-26        int n = grid.length ;
-27        int m = grid[0].length ;
-28
-29        for( int i =0 ; i<4 ; i++){
-30            int nrow = row+ drow[i] ;
-31            int ncol = col + dcol[i];
-32
-33            if( nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1' ){
-34                dfs( grid , vis , nrow , ncol );
-35            }
-36        }
-37    }
-38}
+21    public void dfs( char[][] grid , int r , int c , boolean vis[][]){
+22        if( grid[r][c]=='0' || vis[r][c] ){
+23            return  ;
+24        }
+25        vis[r][c] = true ;
+26
+27        int row[] = { -1 , 0 , 1 , 0} ;
+28        int col[] = {  0, 1 , 0 , -1 } ;
+29
+30        for( int i = 0 ; i<4 ; i++ ) {
+31            int nrow = r+ row[i] ;
+32            int ncol = c + col[i] ;
+33
+34            if( nrow>=0 && ncol>=0 && nrow<grid.length && ncol<grid[0].length && !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
+35                dfs( grid , nrow , ncol , vis ) ;
+36            }
+37        }           
+38        return  ;
+39    }
+40}
