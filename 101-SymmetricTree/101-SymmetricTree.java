9eaeab300f2b1a4c0d732af92ee570969450cacb
Ram Lal <ramlal0801@gmail.com>
2026-03-12 11:31:58 +0530

    File updated at 12/3/2026, 11:31:59 am

--- 101-SymmetricTree/101-SymmetricTree.java
+++ 101-SymmetricTree/101-SymmetricTree.java
@@ -1,4 +1,4 @@
-// Last updated: 1/3/2026, 12:22:34 am
+// Last updated: 12/3/2026, 11:31:59 am
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -6,7 +6,7 @@
 5 *     TreeNode left;
 6 *     TreeNode right;
 7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }1111111111116-4444225
+8 *     TreeNode(int val) { this.val = val; }
 9 *     TreeNode(int val, TreeNode left, TreeNode right) {
 10 *         this.val = val;
 11 *         this.left = left;
@@ -16,26 +16,24 @@
 15 */
 16class Solution {
 17    public boolean isSymmetric(TreeNode root) {
-18        return isSameTree( root.left , root.right);
-19    }
-20
-21    public boolean isSameTree(TreeNode p, TreeNode q) {
-22       
-23        if( p==null && q==null ) {
+18        if( root==null ) return true ;
+19        return helper( root.left , root.right ) ;
+20    }
+21
+22    public boolean helper( TreeNode p , TreeNode q ){
+23        if( p==null && q==null ){
 24            return true ;
 25        }
 26        if( p==null && q!=null ) return false ;
 27        if( q==null && p!=null ) return false ;
-28        if( p.val != q.val ){
-29            return false ;
-30        }
-31
+28        
+29        if( p.val!=q.val ) return false ;
+30        boolean left = helper( p.left , q.right ) ;
+31        if(!left ) return false ;
 32
-33        boolean left = isSameTree( p.left , q.right ) ;
-34        if( !left) return false ;
-35        boolean right = isSameTree( p.right , q.left );
-36        if( !right) return false ;
-37
-38        return left&& right ;
-39    }
-40}
+33        boolean right = helper( p.right , q.left ) ;
+34        if( !right ) return false ;
+35
+36        return true ;
+37    }
+38}
