4f360dad49aeecfcd1dd9b11a4f711915059a5e7
Ram Lal <ramlal0801@gmail.com>
2026-03-12 11:20:33 +0530

    File updated at 12/3/2026, 11:20:34 am

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 2/3/2026, 2:30:26 pm
+// Last updated: 12/3/2026, 11:20:34 am
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -8,19 +8,22 @@
 7 *     TreeNode(int x) { val = x; }
 8 * }
 9 */
-10class Solution { 
-11    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) { 
-12        if( root==null || root==p || root==q) {
-13            return root ;
-14        }
-15
-16        TreeNode left = lowestCommonAncestor(root.left , p , q);
-17        TreeNode right = lowestCommonAncestor(root.right , p , q);
-18
-19        if( left == null ){
-20            return right ;
-21        }
-22        if( right == null ) return left ;
-23        return root ;
-24}
-25}
+10class Solution {
+11    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+12        
+13        if( root==null || root==p || root==q ){
+14            return root ;
+15        }
+16
+17        TreeNode left = lowestCommonAncestor( root.left , p ,q ) ;
+18        TreeNode right = lowestCommonAncestor( root.right , p , q ) ;
+19
+20        if( left==null ) {
+21            return right ;
+22        }
+23        else if( right==null ){
+24            return left ;
+25        }
+26        else return root ;
+27    }
+28}
