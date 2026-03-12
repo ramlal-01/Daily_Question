bc324f8cf943e592737389eb4719b0f5f61ace4b
Ram Lal <ramlal0801@gmail.com>
2026-03-12 11:07:25 +0530

    File updated at 12/3/2026, 11:07:25 am

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,25 +1,28 @@
-// Last updated: 21/8/2025, 7:53:21 am
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int maxDepth(TreeNode root) {
-        if( root== null){
-            return 0;
-        }
-
-        return 1 + Math.max( maxDepth(root.left) , maxDepth( root.right ) );
-    }
-}
+// Last updated: 12/3/2026, 11:07:25 am
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int maxDepth(TreeNode root) {
+18        if( root == null ){
+19            return 0 ;
+20        }
+21
+22        int left = maxDepth( root.left ) ;
+23        int right = maxDepth( root.right ) ;
+24
+25        return 1 + Math.max( left , right ) ;
+26    }
+27}
