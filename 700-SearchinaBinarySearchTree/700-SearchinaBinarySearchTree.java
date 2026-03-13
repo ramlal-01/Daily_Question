d77d9fb7a0d01a66642b77b59913f09e003a2b15
Ram Lal <ramlal0801@gmail.com>
2026-03-13 10:38:22 +0530

    File updated at 13/3/2026, 10:38:24 am

--- 700-SearchinaBinarySearchTree/700-SearchinaBinarySearchTree.java
+++ 700-SearchinaBinarySearchTree/700-SearchinaBinarySearchTree.java
@@ -1,25 +1,29 @@
-// Last updated: 22/8/2025, 4:45:04 pm
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode searchBST(TreeNode root, int val) {
-        if( root==null) return null;
-
-        if( root.val == val ) return root ;
-        else if ( root.val < val ) return searchBST( root.right , val );
-        else return searchBST( root.left , val );
-    }
-}
+// Last updated: 13/3/2026, 10:38:24 am
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode searchBST(TreeNode root, int val) {
+18        if( root==null ){
+19            return null ;
+20        }
+21        if( root.val == val ) return root  ;
+22
+23        else if( val < root.val ) {
+24            return searchBST(root.left , val ) ;
+25        }
+26        else return searchBST( root.right , val ) ;
+27    }
+28}
