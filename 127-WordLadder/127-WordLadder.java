2459945d21f29c636ac5ca852d4d5717b256a7ca
Ram Lal <ramlal0801@gmail.com>
2026-03-16 11:55:48 +0530

    File updated at 16/3/2026, 11:55:51 am

--- 127-WordLadder/127-WordLadder.java
+++ 127-WordLadder/127-WordLadder.java
@@ -1,52 +1,49 @@
-// Last updated: 3/1/2026, 11:30:10 am
+// Last updated: 16/3/2026, 11:55:51 am
 1class Pair{
-2    String first ;
-3    int second ;
-4    Pair( String f , int s ){
-5        first = f ;
-6        second = s ;
+2    String str ;
+3    int dist ;
+4    Pair( String str , int dist ){
+5        this.str = str ;
+6        this.dist = dist ;
 7    }
 8}
 9class Solution {
 10    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-11        Queue<Pair> q = new LinkedList<>();
-12        
-13        Set<String> set = new HashSet<String>(); 
-14
-15        for( String ele : wordList){
-16            set.add( ele);
-17        }
-18        q.add( new Pair( beginWord,1));
-19        set.remove(beginWord);
-20
-21        if (!set.contains(endWord)) return 0;
-22        
-23        while( !q.isEmpty() ){
-24            Pair curr = q.poll() ;
-25            String word = curr.first ;
-26            int dist = curr.second ;
-27
-28            for(int i =0;i<word.length() ; i++){
-29                char arr[] = word.toCharArray();
-30                for( char ch = 'a' ; ch<='z'  ; ch++ ){
-31
-32                    if (arr[i] == ch) continue;
-33                    arr[i] = ch ;
-34                    String nword = new String(arr);
+11        HashSet<String> set = new HashSet<>() ;
+12        for( String ele : wordList ){
+13            set.add( ele ) ;
+14        }
+15
+16        Queue<Pair> q = new LinkedList<>() ;
+17        q.add( new Pair(beginWord, 1 )) ;
+18        set.remove( beginWord ) ;
+19
+20        if( !set.contains(endWord) ) return 0 ;
+21        
+22        while( !q.isEmpty() ){
+23            String s = q.peek().str ;
+24            int d = q.peek().dist ;
+25            q.poll() ;
+26
+27            for( int i =0 ; i<s.length() ; i++ ){
+28                for( char c = 'a' ; c<='z' ; c++ ){
+29                    char[] arr = s.toCharArray() ;
+30
+31                    if( arr[i]==c ) continue ;
+32
+33                    arr[i] = c ;
+34                    String nword = new String(arr) ;
 35
-36                    if (nword.equals(endWord)) {
-37                        return dist + 1;
-38                    }
-39                    else if( set.contains(nword) ){
-40                        q.add( new Pair( nword , dist+1));
-41                        set.remove(nword);
-42                    }
-43                    
-44                }
-45            }
-46        }
-47
-48        return 0 ;
-49
-50    }
-51}
+36                    if( nword.equals( endWord )) return d+1 ;
+37
+38                    else if( set.contains(nword) ){
+39                        q.add( new Pair(nword, d+1 ) );
+40                        set.remove(nword) ;
+41                    }
+42                }
+43            }
+44            
+45        }
+46        return 0 ;
+47    }
+48}
