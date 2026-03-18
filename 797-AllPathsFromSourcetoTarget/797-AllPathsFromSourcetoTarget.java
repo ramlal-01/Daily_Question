c84b4fd1a69b02304a58ca04ead6fc5b96be8c21
Ram Lal <ramlal0801@gmail.com>
2026-03-18 07:24:37 +0530

    File updated at 18/3/2026, 7:24:38 am

--- 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.java
+++ 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.java
@@ -1,17 +1,29 @@
-// Last updated: 18/3/2026, 7:19:37 am
+// Last updated: 18/3/2026, 7:24:38 am
 1class Solution {
-2    public void dfs( int[][] graph ,int node ,List<List<Integer>> list , List<Integer> ans ){ 
-3        ans.add( node);
-4        for( int ele : graph[node]){ 
-5                dfs( graph , ele , list , ans );
-6        } 
-7        if( node == graph.length -1 ) list.add(new ArrayList<>(ans));
-8        ans.remove( ans.size()-1);
-9    }
-10    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-11        List<List<Integer>> list = new ArrayList<>();  
-12            List<Integer> ans = new ArrayList<>();
-13            dfs( graph   , 0 , list , ans); 
-14        return list ;
-15    }
-16}
+2    public int findCircleNum(int[][] isConnected) {
+3        int n = isConnected.length ; 
+4
+5        boolean vis[] = new boolean[n] ;
+6        int c = 0 ;
+7        for( int i =0 ; i<n ; i++ ){
+8            if( !vis[i] ){
+9                dfs( isConnected , i , vis );
+10                c++;
+11            }
+12        }
+13        return c ;
+14    }
+15
+16    public void dfs( int[][] isConnected , int node , boolean[] vis ){
+17        vis[node] = true ;
+18
+19        int n = isConnected[node].length ;
+20        for( int i = 0 ; i <n ; i++){
+21            if( !vis[i] && isConnected[node][i]==1 ){
+22                dfs( isConnected , i , vis );
+23            }
+24        }
+25    }
+26
+27
+28}
