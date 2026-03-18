5da02a1e8882ff5e793226be7cdfc8fa3abe9bbd
Ram Lal <ramlal0801@gmail.com>
2026-03-18 07:19:36 +0530

    File updated at 18/3/2026, 7:19:37 am

--- 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.java
+++ 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.java
@@ -1,17 +1,17 @@
-// Last updated: 21/9/2025, 11:05:12 pm
-class Solution {
-    public void dfs( int[][] graph  , int node ,    List<List<Integer>> list , List<Integer> ans ){ 
-        ans.add( node);
-        for( int ele : graph[node]){ 
-                dfs( graph , ele , list , ans );
-        } 
-        if( node == graph.length -1 ) list.add(new ArrayList<>(ans));
-        ans.remove( ans.size()-1);
-    }
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> list = new ArrayList<>();  
-            List<Integer> ans = new ArrayList<>();
-            dfs( graph   , 0 , list , ans); 
-        return list ;
-    }
-}
+// Last updated: 18/3/2026, 7:19:37 am
+1class Solution {
+2    public void dfs( int[][] graph ,int node ,List<List<Integer>> list , List<Integer> ans ){ 
+3        ans.add( node);
+4        for( int ele : graph[node]){ 
+5                dfs( graph , ele , list , ans );
+6        } 
+7        if( node == graph.length -1 ) list.add(new ArrayList<>(ans));
+8        ans.remove( ans.size()-1);
+9    }
+10    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+11        List<List<Integer>> list = new ArrayList<>();  
+12            List<Integer> ans = new ArrayList<>();
+13            dfs( graph   , 0 , list , ans); 
+14        return list ;
+15    }
+16}
