619c37b70812b3a7ed4e88563f3b1f6017f56ecf
Ram Lal <ramlal0801@gmail.com>
2026-03-18 12:05:36 +0530

    File updated at 18/3/2026, 12:05:37 pm

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,36 +1,40 @@
-// Last updated: 31/12/2025, 12:30:26 pm
+// Last updated: 18/3/2026, 12:05:37 pm
 1class Solution {
-2    public boolean canFinish(int numCourses, int[][] prerequisites) {
-3        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-4        int V = numCourses;
-5        for( int i =0 ; i<V ; i++){
-6            adj.add( new ArrayList<>());
-7        }
-8        int indegree[] = new int[V];
-9        for( int i =0 ; i<prerequisites.length ; i++){
-10            adj.get( prerequisites[i][0]).add(prerequisites[i][1]);
-11            indegree[ prerequisites[i][1]]++;
+2    public boolean canFinish(int n, int[][] arr) {
+3        List<List<Integer>> adj = new ArrayList<>() ;
+4        for( int i =0 ; i<n ; i++ ) adj.add( new ArrayList<>()) ;
+5
+6        int ind[] = new int[n] ;
+7
+8        for( int ele[] : arr ){
+9            adj.get(ele[0]).add(ele[1]);
+10            adj.get(ele[1]).add(ele[0]);
+11            ind[ele[1]]++;
 12        }
-13        Queue<Integer> q = new ArrayDeque<>();
-14        for(int i = 0 ; i<V ; i++){
-15            if( indegree[i]==0){
-16                q.add( i);
-17            }
-18        } 
-19        int c =0;
-20        while( !q.isEmpty()){
-21            int node = q.peek();
-22            q.poll(); 
-23            c++;            
-24            for( int ele : adj.get( node)){
-25                indegree[ele]--;
-26                
-27                if( indegree[ele]==0){
-28                    q.add( ele );
-29                }
-30            }
-31        }
-32
-33        return c==V ;
-34    }
-35}
+13
+14        Queue<Integer> q = new LinkedList<>();
+15
+16        for( int i=0 ; i<n ; i++ ){
+17            if( ind[i]==0 ) q.add(i) ;
+18        }
+19
+20        if( q.isEmpty()) return false ;
+21
+22        while(!q.isEmpty() ){
+23            int node = q.poll() ;
+24
+25            for( int ele : adj.get(node) ){
+26                ind[ele]-- ;
+27
+28                if( ind[ele]==0 ){
+29                    q.add(ele);
+30                }
+31            }
+32        }
+33
+34        for( int ele : ind ){
+35            if( ele>0) return false ;
+36        }
+37        return true ;
+38    }
+39}
