a536ae49573b34a8af07c6aee01931166ce2c293
Ram Lal <ramlal0801@gmail.com>
2026-03-18 08:18:19 +0530

    File updated at 18/3/2026, 8:18:19 am

--- 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.java
+++ 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.java
@@ -1,53 +1,52 @@
-// Last updated: 18/3/2026, 8:05:41 am
+// Last updated: 18/3/2026, 8:18:19 am
 1class Solution {
-2    static void dfs( boolean[][] visited , char[][] mat , int row , int col , int[] delrow , int[] delcol ){
-3        int m = mat.length;
-4        int n = mat[0].length ;
-5        visited[row][col] = true ;
-6        for( int k =0 ; k< 4 ; k++){
-7            int nrow = row+delrow[k];
-8            int ncol = col+ delcol[k];
-9            
-10            if( nrow>=0 && nrow< m && ncol>=0  && ncol <n && !visited[nrow][ncol] 
-11            && mat[nrow][ncol]=='O'){
-12                dfs( visited , mat , nrow , ncol, delrow, delcol );
-13            }
-14        }
-15    }
-16    public void solve(char[][] board) {
-17        int m = board.length;
-18        int n = board[0].length ;
-19        boolean visited[][] = new boolean[m][n];
-20        int delrow[] = { -1 ,0, 1,0};
-21        int delcol[] = { 0 ,1 , 0 , -1};
-22        for( int j = 0 ; j< n ; j++){
-23            
-24            if( !visited[0][j] && board[0][j]=='O'){
-25                dfs( visited , board , 0 , j , delrow, delcol);
+2    int n = 0 ;
+3    int m = 0 ;
+4    int drow[] = { -1,0,1,0} ;
+5    int dcol[] = { 0,1,0,-1} ;
+6    public void solve(char[][] board) {
+7        n = board.length ;
+8        m = board[0].length ;
+9
+10        boolean vis[][] = new boolean[n][m] ;
+11
+12        for( int i =0 ; i<m; i++ ){
+13            if( !vis[0][i] && board[0][i]=='O'){
+14                dfs( board , vis , 0 , i );
+15            }
+16            if( !vis[n-1][i] && board[n-1][i]=='O'){
+17                dfs( board , vis , n-1 , i );
+18            }
+19        }
+20        for( int i =0 ; i<n; i++ ){
+21            if( !vis[i][0] && board[i][0]=='O'){
+22                dfs( board , vis , i , 0 );
+23            }
+24            if( !vis[i][m-1] && board[i][m-1]=='O'){
+25                dfs( board , vis , i , m-1 );
 26            }
-27            
-28            if( !visited[m-1][j] && board[m-1][j]== 'O'){
-29                dfs( visited , board , m-1 , j , delrow, delcol);
-30            }
-31        }
-32        
-33        for( int i =0 ; i< m ; i++){
-34            if( !visited[i][0] && board[i][0] == 'O'){
-35                dfs( visited , board , i , 0, delrow, delcol);
-36            }
-37            if( !visited[i][n-1] && board[i][n-1] == 'O'){
-38                dfs( visited ,  board , i , n-1, delrow, delcol );
-39            }
-40        }
-41        
-42        for( int i=0 ; i<m ; i++){
-43            for( int j=0 ; j<n ; j++){
-44                if( !visited[i][j] && board[i][j]=='O'){
-45                    board[i][j] = 'X';
-46                }
-47            }
-48        } 
-49    }
-50}
-51
-52 
+27        }
+28
+29        for( int i =0 ; i<n ; i++ ){
+30            for( int j=0 ; j<m ; j++ ){
+31                if( !vis[i][j] && board[i][j]=='O' ){
+32                    board[i][j] = 'X' ;
+33                }
+34            }
+35        }
+36    }
+37
+38    public void dfs( char[][] board , boolean[][] vis , int r , int c ){
+39        vis[r][c] = true ;
+40
+41        for( int i = 0 ; i< 4 ; i++ ){
+42            int nrow = r + drow[i] ;
+43            int ncol = c + dcol[i] ;
+44
+45            if( nrow>=0 && ncol>=0 && nrow<n && ncol<m
+46                && board[nrow][ncol]=='O' && !vis[nrow][ncol] ){
+47                dfs( board , vis , nrow , ncol );
+48            }
+49        }
+50    }
+51}
