f13742376cf1b1e08d97c1ac002a55709cbdbe0b
Ram Lal <ramlal0801@gmail.com>
2026-03-18 11:17:26 +0530

    File updated at 18/3/2026, 11:17:26 am

--- 1976-NumberofWaystoArriveatDestination/1976-NumberofWaystoArriveatDestination.java
+++ 1976-NumberofWaystoArriveatDestination/1976-NumberofWaystoArriveatDestination.java
@@ -1,62 +1,52 @@
-// Last updated: 21/9/2025, 8:41:43 pm
-class Pair{
-    int first ;
-    long second ; 
-    Pair( int f , long s){
-        first = f ;
-        second= s ;
-    }
-}
-class Solution {
-    public int countPaths(int n, int[][] roads) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for( int i =0 ; i< n ; i++) adj.add( new ArrayList<>());
-
-        for( int i =0 ; i<roads.length ; i++){
-            int u = roads[i][0];
-            int v = roads[i][1];
-            int w = roads[i][2];
-
-            adj.get(u).add( new Pair(v,w));
-            adj.get(v).add( new Pair(u,w));
-        }
-
-        PriorityQueue<Pair> q = new PriorityQueue<>( (a,b) -> Long.compare(a.second, b.second));
-
-        q.add( new Pair( 0 , 0));
-
-        long dist[] = new long[n];
-        int ways[] = new int[n];
-
-        Arrays.fill(dist, Long.MAX_VALUE);
-
-        Arrays.fill( ways , 0);
-
-        ways[0] = 1 ;
-        dist[0] = 0 ;
-
-        int mod = (int)(1e9 + 7);
-        while( !q.isEmpty()){
-            int node = q.peek().first ;
-            long time = q.peek().second ;
-            q.poll();
-
-            for( Pair ele : adj.get(node) ){
-                int nnode = ele.first ;
-                long ntime = ele.second ;
-
-                long newDist = time + ntime;
-                if (newDist < dist[nnode]) {
-                    dist[nnode] = newDist;
-                    q.add(new Pair(nnode, newDist));
-                    ways[nnode] = ways[node];
-                } else if (newDist == dist[nnode]) {
-                    ways[nnode] = (ways[nnode] + ways[node]) % mod;
-                }
-
-            }
-        }
-
-        return ways[n-1];
-    }
-}
+// Last updated: 18/3/2026, 11:17:26 am
+1class Solution {
+2    public int countPaths(int n, int[][] roads) {
+3        long mod = (long)1e9+7 ;
+4        List<List<long[]>> adj = new ArrayList<>() ;
+5        for( int i=0 ; i<n ; i++ ){
+6            adj.add( new ArrayList<>() );
+7        }
+8
+9        for( int ele[] : roads ){
+10            adj.get( ele[0] ).add( new long[]{ele[1] , ele[2]} );
+11            adj.get( ele[1] ).add( new long[]{ele[0], ele[2]} );
+12        }
+13
+14        long dist[] = new long[n] ;
+15        Arrays.fill( dist , Long.MAX_VALUE );
+16        dist[0] = 0 ;
+17
+18        long path[] = new long[n] ; 
+19        path[0] = 1;
+20
+21        PriorityQueue<long[]> pq = new PriorityQueue<>( (a,b)-> Long.compare(a[1], b[1]) );
+22        pq.add( new long[]{ 0 , 0 }) ;
+23
+24        while( !pq.isEmpty() ){
+25            long[] curr = pq.poll() ;
+26            long node = curr[0] ;
+27            long dis = curr[1] ;
+28
+29            if( dis > dist[(int) node] ) continue ;
+30
+31            for( long[] nei : adj.get((int) node) ){
+32                long nnode = nei[0] ;
+33                long ndist = nei[1] ;
+34
+35                long newdist = dis + ndist ;
+36
+37                if( newdist < dist[(int)nnode] ){
+38
+39                    path[(int)nnode] = path[(int)node]  ;
+40                    dist[(int)nnode] = newdist ;
+41                    pq.add( new long[]{ nnode , newdist } );
+42
+43                }
+44                else if( newdist == dist[(int)nnode] ){
+45                    path[(int)nnode] =( path[ (int)nnode] + path[(int)node] )%mod ;
+46                }
+47            }
+48        }
+49        return (int) (path[n-1]%mod) ;
+50    }
+51}
