2c195bde05c07eb6a566234d9955eb95bf30a32c
Ram Lal <ramlal0801@gmail.com>
2026-03-18 08:05:40 +0530

    File updated at 18/3/2026, 8:05:41 am

--- 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.java
+++ 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.java
@@ -1,67 +1,53 @@
-// Last updated: 17/3/2026, 10:41:13 am
-1class Pair{
-2    int node ;
-3    int dist ; 
-4    Pair( int node , int dist ){
-5        this.node = node ;
-6        this.dist = dist ;
-7    }
-8}
-9class tuple{
-10    int first ;
-11    int second ;
-12    int third ;
-13    tuple( int first , int second , int third ){
-14        this.first = first ;
-15        this.second = second ;
-16        this.third = third ;
-17    }
-18}
-19class Solution {
-20    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-21        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-22        for( int i =0 ; i<n ; i++){
-23            adj.add( new ArrayList<>());
-24        }
-25        
-26        for( int i = 0 ; i<flights.length ; i++){
-27            int u = flights[i][0];
-28            int v = flights[i][1];
-29            int w = flights[i][2];
-30            adj.get(u).add(new Pair( v, w));
+// Last updated: 18/3/2026, 8:05:41 am
+1class Solution {
+2    static void dfs( boolean[][] visited , char[][] mat , int row , int col , int[] delrow , int[] delcol ){
+3        int m = mat.length;
+4        int n = mat[0].length ;
+5        visited[row][col] = true ;
+6        for( int k =0 ; k< 4 ; k++){
+7            int nrow = row+delrow[k];
+8            int ncol = col+ delcol[k];
+9            
+10            if( nrow>=0 && nrow< m && ncol>=0  && ncol <n && !visited[nrow][ncol] 
+11            && mat[nrow][ncol]=='O'){
+12                dfs( visited , mat , nrow , ncol, delrow, delcol );
+13            }
+14        }
+15    }
+16    public void solve(char[][] board) {
+17        int m = board.length;
+18        int n = board[0].length ;
+19        boolean visited[][] = new boolean[m][n];
+20        int delrow[] = { -1 ,0, 1,0};
+21        int delcol[] = { 0 ,1 , 0 , -1};
+22        for( int j = 0 ; j< n ; j++){
+23            
+24            if( !visited[0][j] && board[0][j]=='O'){
+25                dfs( visited , board , 0 , j , delrow, delcol);
+26            }
+27            
+28            if( !visited[m-1][j] && board[m-1][j]== 'O'){
+29                dfs( visited , board , m-1 , j , delrow, delcol);
+30            }
 31        }
-32         
-33        int[] dist = new int[n];
-34        Arrays.fill(dist, Integer.MAX_VALUE);
-35        dist[src] = 0;
-36
-37        Queue<tuple> q = new LinkedList<>();
-38
-39        q.add(new tuple(0,src, 0));
-40
-41        while( !q.isEmpty() ){
-42            tuple curr = q.poll() ;
-43            int stops = curr.first ;
-44            int node  = curr.second ;
-45            int dis   = curr.third ; 
-46
-47            if( stops > k ) continue ;
-48
-49            for( Pair nei : adj.get(node) ){
-50                int nnode = nei.node ;
-51                int ndist = nei.dist ;
-52
-53                int newdist = dis + ndist ;
-54
-55                if( newdist < dist[nnode] ){
-56                    dist[nnode] = newdist ;
-57                    q.add( new tuple( stops+1 , nnode , newdist ) );
-58                }
-59            }
-60        }
-61
-62        if( dist[dst]==Integer.MAX_VALUE ) return -1 ;
-63
-64        return dist[dst] ;
-65    }
-66}
+32        
+33        for( int i =0 ; i< m ; i++){
+34            if( !visited[i][0] && board[i][0] == 'O'){
+35                dfs( visited , board , i , 0, delrow, delcol);
+36            }
+37            if( !visited[i][n-1] && board[i][n-1] == 'O'){
+38                dfs( visited ,  board , i , n-1, delrow, delcol );
+39            }
+40        }
+41        
+42        for( int i=0 ; i<m ; i++){
+43            for( int j=0 ; j<n ; j++){
+44                if( !visited[i][j] && board[i][j]=='O'){
+45                    board[i][j] = 'X';
+46                }
+47            }
+48        } 
+49    }
+50}
+51
+52 
