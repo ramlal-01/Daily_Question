b6c76aa43e282287af848ebaa843f642516fcac5
Ram Lal <ramlal0801@gmail.com>
2026-03-18 12:14:02 +0530

    File updated at 18/3/2026, 12:14:03 pm

--- 210-CourseScheduleII/210-CourseScheduleII.java
+++ 210-CourseScheduleII/210-CourseScheduleII.java
@@ -1,37 +1,42 @@
-// Last updated: 28/2/2026, 1:18:54 am
-1/*
-2// Definition for a Node.
-3class Node {
-4    public int val;
-5    public Node prev;
-6    public Node next;
-7    public Node child;
-8};
-9*/
-10
-11class Solution {
-12    public Node flatten(Node head) { 
-13        Node temp = head ;
-14
-15        Stack<Node> st = new Stack<>() ;
-16
-17        while( temp!=null ){
-18
-19            if( temp.child!=null ) 
-20            {
-21                if( temp.next!=null ) st.push( temp.next ) ;
-22
-23                temp.child.prev = temp ;
-24                temp.next = temp.child ;
-25                temp.child = null ;
-26            }
-27            if( temp.next == null && !st.isEmpty() ) {
-28                Node curr = st.pop() ;
-29                temp.next = curr ;
-30                curr.prev = temp ;
-31            }
-32            temp =  temp.next ;
-33        }
-34        return head ;
-35    }
-36}
+// Last updated: 18/3/2026, 12:14:03 pm
+1class Solution {
+2    public int[] findOrder(int n, int[][] arr ) {
+3        List<List<Integer>> adj = new ArrayList<>() ;
+4        for( int i =0 ; i<n ; i++ ) adj.add( new ArrayList<>()) ;
+5
+6        int ind[] = new int[n] ;
+7
+8        for( int ele[] : arr ){
+9            adj.get(ele[0]).add(ele[1]);
+10            adj.get(ele[1]).add(ele[0]);
+11            ind[ele[0]]++;
+12        }
+13
+14        Queue<Integer> q = new LinkedList<>();
+15
+16        for( int i=0 ; i<n ; i++ ){
+17            if( ind[i]==0 ) q.add(i) ;
+18        }
+19
+20        if( q.isEmpty()) return new int[]{} ;
+21
+22        int topo[] = new int[n];
+23        int k = 0 ;
+24        while(!q.isEmpty() ){
+25            int node = q.poll() ;
+26            topo[k++] = node ;
+27            for( int ele : adj.get(node) ){
+28                ind[ele]-- ;
+29
+30                if( ind[ele]==0 ){
+31                    q.add(ele);
+32                }
+33            }
+34        }
+35
+36        for( int ele : ind ){
+37            if( ele>0) return new int[]{} ;
+38        }
+39        return topo ;
+40    }
+41}
