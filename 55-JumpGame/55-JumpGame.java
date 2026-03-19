1d34eb6e34defba16f8785c8bffe1836084fe09f
Ram Lal <ramlal0801@gmail.com>
2026-03-19 20:11:31 +0530

    File updated at 19/3/2026, 8:11:31 pm

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,15 +1,18 @@
-// Last updated: 18/3/2026, 3:56:14 pm
+// Last updated: 19/3/2026, 8:11:31 pm
 1class Solution {
 2    public boolean canJump(int[] nums) {
 3        int n = nums.length ;
-4        if( nums.length == 1 ) return true ;
+4
 5        int max = 0 ;
-6        for( int i=0 ; i<n ; i++ ){
-7            if( i> max ) return false ;
-8
-9            max  = Math.max( max , i+nums[i]);
-10            if( max >= n-1) return true ;
-11        }
-12        return false ;
-13    }
-14}  
+6
+7        for( int i = 0 ; i<n ; i++ ){
+8            if( i>max ) return false ;
+9
+10            max = Math.max( max , i+nums[i] );
+11
+12            if( max>= n-1 ) return true ;
+13        }
+14
+15        return false ;
+16    }
+17}
