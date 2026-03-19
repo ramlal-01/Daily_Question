3f8eb03f0d12ac8bc82ef7c19ad4e40f2c08f939
Ram Lal <ramlal0801@gmail.com>
2026-03-19 20:49:00 +0530

    File updated at 19/3/2026, 8:49:00 pm

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,18 +1,28 @@
-// Last updated: 19/3/2026, 8:11:31 pm
+// Last updated: 19/3/2026, 8:49:00 pm
 1class Solution {
-2    public boolean canJump(int[] nums) {
-3        int n = nums.length ;
-4
-5        int max = 0 ;
-6
-7        for( int i = 0 ; i<n ; i++ ){
-8            if( i>max ) return false ;
-9
-10            max = Math.max( max , i+nums[i] );
-11
-12            if( max>= n-1 ) return true ;
-13        }
+2    public int[][] merge(int[][] arr ) {
+3
+4        Arrays.sort( arr , (a,b)-> Integer.compare(a[0],b[0]) );
+5        int n = arr.length ;
+6        List<int[]> list = new ArrayList<>() ;
+7
+8        int ps = arr[0][0] ;
+9        int pe = arr[0][1] ;
+10
+11        for( int i = 1 ; i<n ; i++ ){
+12            int cs = arr[i][0] ;
+13            int ce = arr[i][1] ;
 14
-15        return false ;
-16    }
-17}
+15            if( cs <= pe ){
+16                pe = Math.max(pe , ce );
+17            }
+18            else{
+19                list.add( new int[]{ps , pe });
+20                ps = cs ;
+21                pe = ce ;
+22            }
+23        }
+24        list.add( new int[]{ps , pe });
+25        return list.toArray(new int[list.size()][]);
+26    }
+27}
