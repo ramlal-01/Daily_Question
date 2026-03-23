85871c95fa93b0612b475aae2230cbfa56b69d54
Ram Lal <ramlal0801@gmail.com>
2026-03-23 15:22:46 +0530

    File updated at 23/3/2026, 3:22:47 pm

--- 621-TaskScheduler/621-TaskScheduler.java
+++ 621-TaskScheduler/621-TaskScheduler.java
@@ -1,4 +1,4 @@
-// Last updated: 22/3/2026, 11:21:03 pm
+// Last updated: 23/3/2026, 3:22:47 pm
 1class Solution {
 2    public int leastInterval(char[] tasks, int n) {
 3
@@ -9,27 +9,28 @@
 8
 9        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 10
-11        for (int f : freq) {
-12            if (f > 0) pq.add(f);
-13        }
-14        int time = 0;
-15
-16        while (!pq.isEmpty()) {
-17            List<Integer> temp = new ArrayList<>();
-18            int cycle = n + 1;
-19
-20            while (cycle > 0 && !pq.isEmpty()) {
-21                int f = pq.poll();
-22                if (f - 1 > 0) temp.add(f - 1);
-23                time++;
-24                cycle--;
+11        for( int ele : freq ){
+12            if( ele>0 ){
+13                pq.add(ele);
+14            }
+15        }
+16        int time = 0 ;
+17        while( !pq.isEmpty() ){
+18            List<Integer> temp = new ArrayList<>() ;
+19            for( int i=1 ; i<=n+1 ; i++ ){
+20                if( !pq.isEmpty() ){
+21                    int curr = pq.poll() ;
+22                    curr--;
+23                    temp.add(curr) ;
+24                }
 25            }
-26            for (int t : temp) pq.add(t);
-27
-28            if (pq.isEmpty()) break;
-29            time += cycle;  
-30        }
+26
+27            for( int ele : temp ) if( ele>0 ) pq.add(ele) ;
+28
+29            if( pq.isEmpty() ) time+= temp.size() ;
+30            else time+= n+1 ;
 31
-32        return time;
-33    }
-34}
+32        }
+33        return time ;
+34    }
+35}
