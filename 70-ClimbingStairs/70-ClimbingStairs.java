b3caf0180a28e7fbd147db96be083f8ceb5d02c1
Ram Lal <ramlal0801@gmail.com>
2026-03-23 14:32:46 +0530

    File updated at 23/3/2026, 2:32:47 pm

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,14 +1,15 @@
-// Last updated: 20/10/2025, 12:38:45 am
-class Solution {
-    public int climbStairs(int n) {
-        int prev1 = 1 , prev = 1 ;
-        int curr = 1 ;
-        for( int i = 2 ; i<=n ; i++){
-            curr = prev1 + prev ;
-
-            prev1 = prev ;
-            prev = curr ;
-        }
-        return curr ;
-    }
-}
+// Last updated: 23/3/2026, 2:32:47 pm
+1class Solution {
+2    public int climbStairs(int n) {
+3        if( n<=2 ) return n ;
+4
+5        int prev1 =1  , prev = 2 ;
+6        int curr = 1 ;
+7        for( int i =3 ; i<=n ; i++ ){
+8            curr = prev1 + prev ; 
+9            prev1= prev ;
+10            prev = curr ;
+11        }
+12        return curr ;
+13    }
+14}
