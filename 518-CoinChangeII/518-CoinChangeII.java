abc29d0e21c3589101dd6aceb31083171b94b3f5
Ram Lal <ramlal0801@gmail.com>
2026-03-26 10:53:43 +0530

    File updated at 26/3/2026, 10:53:43 am

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,20 +1,19 @@
-// Last updated: 28/10/2025, 10:38:59 am
-class Solution {
-    public int change(int amount, int[] coins) {
-        int m = coins.length ;
-        int n = amount + 1 ;
-
-        int dp[] = new int[n];
-         
-        dp[0] = 1 ;
-        
-        for( int i=0 ; i<m ; i++){
-            // backward  for bounded knapsack
-            for( int j = coins[i] ; j<= amount ; j++ ){
-                dp[j] = dp[j] + dp[j - coins[i]];
-            }
-        }
-        
-        return dp[amount];
-    }
-}
+// Last updated: 26/3/2026, 10:53:43 am
+1class Solution {
+2    public int change(int amount, int[] coins) {
+3        int n = coins.length ;
+4        int dp[][] = new int[n+1][amount+1] ;
+5
+6        dp[0][0] = 1 ;
+7
+8        for( int i = 1 ; i<=n ; i++ ){
+9            for( int j = 0 ; j<=amount ; j++ ){
+10                if( coins[i-1]<=j ){
+11                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j] ;
+12                }
+13                else dp[i][j] = dp[i-1][j] ;
+14            }
+15        }
+16        return dp[n][amount] ;
+17    }
+18}
