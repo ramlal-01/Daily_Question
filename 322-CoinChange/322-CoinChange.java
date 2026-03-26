855fb4d81285ac87a5c5517a6a3941f6c94eccdb
Ram Lal <ramlal0801@gmail.com>
2026-03-26 10:41:09 +0530

    File updated at 26/3/2026, 10:41:09 am

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,26 +1,27 @@
-// Last updated: 28/10/2025, 11:02:52 am
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int m = coins.length ;
-        int n = amount + 1 ;
-
-        int dp[] = new int[n];
-        int max  = n ;
-
-        Arrays.fill( dp , max);
-
-        dp[0] = 0 ;
-        
-        for( int i=0 ; i<m ; i++){
-            // backward  for bounded knapsack
-            for( int j = coins[i] ; j<= amount ; j++ ){
-                dp[j] = Math.min( dp[j] , 1 +  dp[j - coins[i]] );
-                
-            }
-
-            
-        }
-        
-        return dp[amount]==max ? -1 : dp[amount];
-    }
-}
+// Last updated: 26/3/2026, 10:41:09 am
+1class Solution {
+2    public int coinChange(int[] nums, int sum) {
+3        int n = nums.length;
+4        int INF = (int)1e9;
+5
+6        int[][] dp = new int[n + 1][sum + 1];
+7
+8        for (int j = 1; j <= sum; j++) {
+9            dp[0][j] = INF;
+10        }
+11
+12        for (int i = 1; i <= n; i++) {
+13            for (int j = 0; j <= sum; j++) {
+14                
+15                dp[i][j] = dp[i - 1][j];
+16
+17                if (nums[i - 1] <= j) {
+18                    dp[i][j] = Math.min(dp[i][j],
+19                        1 + dp[i][j - nums[i - 1]]);
+20                }
+21            }
+22        }
+23
+24        return dp[n][sum] >= INF ? -1 : dp[n][sum];
+25    }
+26}
