7f76242ba48149261d698be3629c3456bcdfa7f0
Ram Lal <ramlal0801@gmail.com>
2026-03-27 22:57:12 +0530

    File updated at 27/3/2026, 10:57:11 pm

--- 72-EditDistance/72-EditDistance.java
+++ 72-EditDistance/72-EditDistance.java
@@ -1,34 +1,25 @@
-// Last updated: 4/11/2025, 2:37:26 pm
-class Solution {
-    public int findNumberOfLIS(int[] nums) {
-        int n = nums.length ; 
-        int dp[] = new int[n];
-        int count[] = new int[n];
-
-        Arrays.fill( count , 1 );
-        Arrays.fill( dp , 1 );
-
-        int max = 1 ;
-        for( int i = 1 ; i< n ;i++){
-            for( int j = 0 ; j<i ; j++){
-                if( nums[i] > nums[j] ){
-                    if( dp[j]+1 > dp[i]) {
-                        dp[i] = 1 + dp[j] ;
-                        count[i] = count[j];
-                    }
-                    else if( dp[j]+1 == dp[i] ){
-                        count[i]+=count[j];
-                    }
-                }
-            }
-            max = Math.max( max , dp[i]);
-        }
-        
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == max) ans += count[i];
-        }
-
-        return ans;
-    }
-}
+// Last updated: 27/3/2026, 10:57:11 pm
+1class Solution {
+2    public int minDistance(String word1, String word2) {
+3        int m = word1.length();
+4        int n = word2.length();
+5
+6        int dp[][] = new int[m+1][n+1];
+7
+8        // base cases
+9        for (int i = 0; i <= m; i++) dp[i][0] = i; // delete all chars
+10        for (int j = 0; j <= n; j++) dp[0][j] = j; // insert all chars
+11        
+12        for( int i =1 ; i<=m ; i++){
+13            for( int j = 1 ; j<=n ; j++){
+14                if( word1.charAt(i-1)==word2.charAt(j-1)){
+15                    dp[i][j] = dp[i-1][j-1];
+16                }
+17                else{
+18                    dp[i][j] =1 + Math.min( Math.min(dp[i-1][j],dp[i-1][j-1]) , dp[i][j-1]);
+19                }
+20            }
+21        }
+22        return dp[m][n];
+23    }
+24}
