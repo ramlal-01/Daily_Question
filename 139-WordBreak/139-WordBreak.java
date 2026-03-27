513c7b73a22e0a53095a31e68d25ce1a3bea618f
Ram Lal <ramlal0801@gmail.com>
2026-03-27 23:01:58 +0530

    File updated at 27/3/2026, 11:01:57 pm

--- 139-WordBreak/139-WordBreak.java
+++ 139-WordBreak/139-WordBreak.java
@@ -1,34 +1,30 @@
-// Last updated: 27/3/2026, 11:01:15 pm
+// Last updated: 27/3/2026, 11:01:57 pm
 1class Solution {
-2    public int rob(int[] nums) {
-3        int prev1 =0 ;
-4        int prev = nums[0];
-5        int n = nums.length;
-6        if (n == 1) return nums[0];
-7        if (n == 2) return Math.max(nums[0], nums[1]);
-8
-9        for( int i = 1 ; i<n-1 ; i++){
-10            int take = nums[i] ;
-11            if( i>1 ) take+= prev1 ;
-12
-13            int nottake = prev ;
-14            int curr = Math.max( take  , nottake);
-15            prev1 = prev ;
-16            prev = curr ;
-17        }
-18        // System.out.println(prev);
-19        int second =0 ;
-20        int first = nums[1];
-21
-22        for( int i = 2 ; i<n ; i++){
-23            int take = nums[i] + second ;
-24
-25            int nottake = first ;
-26            int curr = Math.max( take  , nottake);
-27            second = first ;
-28            first  = curr ;
-29        }
-30        // System.out.println(first);
-31        return Math.max( prev , first );
-32    }
-33}
+2    int count  = 0 ;
+3    public int countArrangement(int n) { 
+4        boolean arr[] = new boolean[n+1];
+5        helper( n , arr , 1);
+6        
+7        // System.out.println(res);
+8        return count;
+9    }
+10
+11    public void helper( int n  , boolean[] arr , int ind ){
+12        if(  ind>n){
+13            count++;
+14            return ;
+15        }
+16        
+17
+18        for( int i = 1 ; i<=n ; i++){
+19            if( arr[i]) continue ;
+20
+21            arr[i] = true ; 
+22
+23            if( i% ind==0 || ind%i==0 ){ 
+24                helper( n , arr , ind+1); 
+25            }
+26            arr[i] = false;
+27        }
+28    }
+29}
