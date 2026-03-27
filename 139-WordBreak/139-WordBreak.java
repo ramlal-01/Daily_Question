8563a372f3cdc8998608f569bc7f7ec4638ff88c
Ram Lal <ramlal0801@gmail.com>
2026-03-27 23:01:16 +0530

    File updated at 27/3/2026, 11:01:15 pm

--- 139-WordBreak/139-WordBreak.java
+++ 139-WordBreak/139-WordBreak.java
@@ -1,17 +1,34 @@
-// Last updated: 27/3/2026, 11:00:58 pm
+// Last updated: 27/3/2026, 11:01:15 pm
 1class Solution {
-2    public boolean wordBreak(String s, List<String> wordDict) {
-3        int n = s.length();
-4        boolean dp[] = new boolean[n+1] ;
-5        dp[0] = true ;
-6        for (int i = 1; i <= n; i++) {
-7            for (int j = 0; j < i; j++) {
-8                if (dp[j] && wordDict.contains(s.substring(j, i))) {
-9                    dp[i] = true;
-10                    break;
-11                }
-12            }
-13        }
-14        return dp[n];
-15    }
-16}
+2    public int rob(int[] nums) {
+3        int prev1 =0 ;
+4        int prev = nums[0];
+5        int n = nums.length;
+6        if (n == 1) return nums[0];
+7        if (n == 2) return Math.max(nums[0], nums[1]);
+8
+9        for( int i = 1 ; i<n-1 ; i++){
+10            int take = nums[i] ;
+11            if( i>1 ) take+= prev1 ;
+12
+13            int nottake = prev ;
+14            int curr = Math.max( take  , nottake);
+15            prev1 = prev ;
+16            prev = curr ;
+17        }
+18        // System.out.println(prev);
+19        int second =0 ;
+20        int first = nums[1];
+21
+22        for( int i = 2 ; i<n ; i++){
+23            int take = nums[i] + second ;
+24
+25            int nottake = first ;
+26            int curr = Math.max( take  , nottake);
+27            second = first ;
+28            first  = curr ;
+29        }
+30        // System.out.println(first);
+31        return Math.max( prev , first );
+32    }
+33}
