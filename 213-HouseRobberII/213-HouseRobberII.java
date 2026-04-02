cd73b8244ba7d608c604573ebc827c2b05a297f6
Ram Lal <ramlal0801@gmail.com>
2026-04-02 22:56:34 +0530

    File updated at 2/4/2026, 10:56:37 pm

--- 213-HouseRobberII/213-HouseRobberII.java
+++ 213-HouseRobberII/213-HouseRobberII.java
@@ -1,34 +1,34 @@
-// Last updated: 20/10/2025, 2:40:49 pm
-class Solution {
-    public int rob(int[] nums) {
-        int prev1 =0 ;
-        int prev = nums[0];
-        int n = nums.length;
-        if (n == 1) return nums[0];
-        if (n == 2) return Math.max(nums[0], nums[1]);
-
-        for( int i = 1 ; i<n-1 ; i++){
-            int take = nums[i] ;
-            if( i>1 ) take+= prev1 ;
-
-            int nottake = prev ;
-            int curr = Math.max( take  , nottake);
-            prev1 = prev ;
-            prev = curr ;
-        }
-        // System.out.println(prev);
-        int second =0 ;
-        int first = nums[1];
-
-        for( int i = 2 ; i<n ; i++){
-            int take = nums[i] + second ;
-
-            int nottake = first ;
-            int curr = Math.max( take  , nottake);
-            second = first ;
-            first  = curr ;
-        }
-        // System.out.println(first);
-        return Math.max( prev , first );
-    }
-}
+// Last updated: 2/4/2026, 10:56:37 pm
+1class Solution {
+2    public int rob(int[] nums) {
+3        int prev1 =0 ;
+4        int prev = nums[0];
+5        int n = nums.length;
+6        if (n == 1) return nums[0];
+7        if (n == 2) return Math.max(nums[0], nums[1]);
+8
+9        for( int i = 1 ; i<n-1 ; i++){
+10            int take = nums[i] ;
+11            if( i>1 ) take+= prev1 ;
+12
+13            int nottake = prev ;
+14            int curr = Math.max( take  , nottake);
+15            prev1 = prev ;
+16            prev = curr ;
+17        }
+18        // System.out.println(prev);
+19        int second =0 ;
+20        int first = nums[1];
+21
+22        for( int i = 2 ; i<n ; i++){
+23            int take = nums[i] + second ;
+24
+25            int nottake = first ;
+26            int curr = Math.max( take  , nottake);
+27            second = first ;
+28            first  = curr ;
+29        }
+30        // System.out.println(first);
+31        return Math.max( prev , first );
+32    }
+33}
