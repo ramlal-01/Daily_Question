7f3e954c4b26b83a42863a6a56c7745597fe843c
Ram Lal <ramlal0801@gmail.com>
2026-04-09 15:30:36 +0530

    File updated at 9/4/2026, 3:30:36 pm

--- 141-LinkedListCycle/141-LinkedListCycle.java
+++ 141-LinkedListCycle/141-LinkedListCycle.java
@@ -1,26 +1,45 @@
-// Last updated: 9/4/2026, 2:52:14 pm
+// Last updated: 9/4/2026, 3:30:36 pm
 1/**
 2 * Definition for singly-linked list.
-3 * class ListNode {
+3 * public class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode(int x) {
-7 *         val = x;
-8 *         next = null;
-9 *     }
-10 * }
-11 */
-12public class Solution {
-13    public boolean hasCycle(ListNode head) {
-14        ListNode slow = head ;
-15        ListNode fast = head ;
-16
-17        while( fast!=null && fast.next!=null ){
-18            slow = slow.next ;
-19            fast = fast.next.next ;
-20
-21            if( slow==fast ) return true ;
-22        }
-23        return false ;
-24    }
-25}
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode mergeKLists(ListNode[] lists) {
+13
+14        PriorityQueue<ListNode> pq = new PriorityQueue<>( (a,b)-> a.val - b.val ) ;
+15
+16        for( ListNode curr : lists ){
+17            if( curr!=null ){
+18                pq.add( curr) ;
+19            }
+20        }
+21
+22        ListNode dummy = new ListNode(0) ;
+23
+24        ListNode node = dummy ;
+25        
+26        while( !pq.isEmpty() ){
+27            ListNode curr = pq.poll() ;
+28
+29            System.out.println( curr.val ) ;
+30
+31            ListNode nnode = new ListNode(curr.val ) ;
+32
+33            dummy.next = nnode ;
+34            dummy = dummy.next  ;
+35            
+36            if( curr.next!=null ){
+37                pq.add( curr.next ) ;
+38            }
+39
+40        }
+41
+42        return node.next ;
+43    }
+44}
