1dfc37cf4d9da64cdcc5f2d21c3b5b1dcdaa5985
Ram Lal <ramlal0801@gmail.com>
2026-04-14 23:27:33 +0530

    Create 648-ReplaceWords

---
+++ 648-ReplaceWords/648-ReplaceWords.java
@@ -0,0 +1,26 @@
+// Last updated: 14/4/2026, 11:27:33 pm
+1class Solution {
+2    public String replaceWords(List<String> dictionary, String sentence) {
+3
+4        HashSet<String> set = new HashSet<>(dictionary);
+5
+6        String[] words = sentence.split(" ");
+7        StringBuilder result = new StringBuilder();
+8
+9        for (String word : words) {
+10            String replacement = word;
+11
+12            for (int i = 1; i <= word.length(); i++) {
+13                String prefix = word.substring(0, i);
+14                if (set.contains(prefix)) {
+15                    replacement = prefix;
+16                    break; // shortest mil gaya
+17                }
+18            }
+19
+20            result.append(replacement).append(" ");
+21        }
+22
+23        return result.toString().trim();
+24    }
+25}
