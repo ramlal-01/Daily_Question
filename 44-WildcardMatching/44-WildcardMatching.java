4bc4f5897162060d77c7ae93173602073ff8efb2
Ram Lal <ramlal0801@gmail.com>
2026-04-25 22:54:53 +0530

    File updated at 25/4/2026, 10:54:53 pm

--- 44-WildcardMatching/44-WildcardMatching.java
+++ 44-WildcardMatching/44-WildcardMatching.java
@@ -1,38 +1,38 @@
-// Last updated: 7/10/2025, 4:40:00 am
-class Solution {
-    public boolean isMatch(String s, String p) {
-        int i = s.length();
-        int j = p.length();
-
-        Boolean dp[][] = new Boolean[i][j];
-
-        // for( int ele[] : dp) Arrays.fill( ele , -1 );
-
-        return match( s,p , i-1 , j-1 , dp);
-
-    }
-
-    public boolean match( String s , String p , int i , int j , Boolean dp[][]){
-        if( i<0 && j<0) return true ;
-
-        if( j<0 && i>=0 ) return false ;
-
-        if( i<0 && j>=0) {
-            for( int k=0 ; k<=j  ; k++){
-                if( p.charAt(k)!='*') return false;
-            }
-            return true;
-        }
-
-        if( dp[i][j]!=null) return dp[i][j];
-
-        if( s.charAt(i)== p.charAt(j) || p.charAt(j)=='?'){
-            return dp[i][j] = match( s, p , i-1 , j-1 ,dp);
-        }
-
-        if( p.charAt(j)=='*'){
-            return dp[i][j] = match( s, p , i-1,j ,dp) || match( s, p , i , j-1 , dp );
-        }
-        return dp[i][j] = false;
-    }
-}
+// Last updated: 25/4/2026, 10:54:53 pm
+1class Solution {
+2    public boolean isMatch(String s, String p) {
+3        int i = s.length();
+4        int j = p.length();
+5
+6        Boolean dp[][] = new Boolean[i][j];
+7
+8        // for( int ele[] : dp) Arrays.fill( ele , -1 );
+9
+10        return match( s,p , i-1 , j-1 , dp);
+11
+12    }
+13
+14    public boolean match( String s , String p , int i , int j , Boolean dp[][]){
+15        if( i<0 && j<0) return true ;
+16
+17        if( j<0 && i>=0 ) return false ;
+18
+19        if( i<0 && j>=0) {
+20            for( int k=0 ; k<=j  ; k++){
+21                if( p.charAt(k)!='*') return false;
+22            }
+23            return true;
+24        }
+25
+26        if( dp[i][j]!=null) return dp[i][j];
+27
+28        if( s.charAt(i)== p.charAt(j) || p.charAt(j)=='?'){
+29            return dp[i][j] = match( s, p , i-1 , j-1 ,dp);
+30        }
+31
+32        if( p.charAt(j)=='*'){
+33            return dp[i][j] = match( s, p , i-1,j ,dp) || match( s, p , i , j-1 , dp );
+34        }
+35        return dp[i][j] = false;
+36    }
+37}
