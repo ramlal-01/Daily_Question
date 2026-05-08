61e7a7f121bda1f59a5c3c83f4fbd33a3c1a21af
Ram Lal <ramlal0801@gmail.com>
2026-05-09 00:10:02 +0530

    File updated at 9/5/2026, 12:10:03 am

--- 22-GenerateParentheses/22-GenerateParentheses.java
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -1,24 +1,24 @@
-// Last updated: 6/10/2025, 8:24:14 am
-class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-
-        func( "" , n ,res , 0 , 0 );
-
-        return res ;
-    }
-
-    public void func( String curr , int n ,List<String> res , int open , int close){
-        if( curr.length()==2*n){
-            res.add(curr);
-            return ;
-        }
-
-        if( open<n){
-            func( curr+"(" , n , res , open+1 , close);
-        }
-        if( close<open){
-            func( curr+ ")" , n , res , open , close+1);
-        }
-    }
-}
+// Last updated: 9/5/2026, 12:10:03 am
+1class Solution {
+2    public List<String> generateParenthesis(int n) {
+3        List<String> res = new ArrayList<>();
+4
+5        func( "" , n ,res , 0 , 0 );
+6
+7        return res ;
+8    }
+9
+10    public void func( String curr , int n ,List<String> res , int open , int close){
+11        if( curr.length()==2*n){
+12            res.add(curr);
+13            return ;
+14        }
+15
+16        if( open<n){
+17            func( curr+"(" , n , res , open+1 , close);
+18        }
+19        if( close<open){
+20            func( curr+ ")" , n , res , open , close+1);
+21        }
+22    }
+23}
