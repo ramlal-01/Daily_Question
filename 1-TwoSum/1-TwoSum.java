349830ee4fae98d6f7ae9455fb98ed24416962a5
Ram Lal <ramlal0801@gmail.com>
2026-05-16 13:35:26 +0530

    File updated at 16/5/2026, 1:35:28 pm

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,19 +1,17 @@
-// Last updated: 30/4/2026, 9:22:01 pm
+// Last updated: 16/5/2026, 1:35:28 pm
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
-3        int n = nums.length ;
-4
-5        HashMap<Integer,Integer> map = new HashMap<>() ;
-6        int ans[] = new int[2] ;
+3        int n  = nums.length ;
+4        int t = target ; 
+5
+6        HashMap<Integer,Integer> map = new HashMap<>() ;
 7
-8        for( int i =0 ; i<n ; i++ ){
-9            if( map.containsKey(target-nums[i])){
-10                ans[0]= map.get(target-nums[i]);
-11                ans[1] = i ;
-12                break;
-13            }
-14            map.put( nums[i] , i );
-15        }
-16        return ans ;
-17    }
-18}
+8        for( int i = 0 ; i<n ; i++ ){
+9            if( map.containsKey( target - nums[i]) ){
+10                return new int[]{ map.get( target-nums[i] ),i } ;
+11            }
+12            map.put( nums[i] , i );
+13        }
+14        return new int[]{} ;
+15    }
+16}
