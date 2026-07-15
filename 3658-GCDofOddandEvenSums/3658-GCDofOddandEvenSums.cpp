fb75617a9afddce7c14e64cacafefa4ce2ad9824
Ram Lal <ramlal0801@gmail.com>
2026-07-15 23:20:52 +0530

    File updated at 15/7/2026, 11:20:52 pm

--- 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.cpp
+++ 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.cpp
@@ -1,24 +1,8 @@
-// Last updated: 15/7/2026, 11:14:24 pm
+// Last updated: 15/7/2026, 11:20:52 pm
 1class Solution {
 2public:
 3    int gcdOfOddEvenSums(int n) {
-4        int odd = 0 ;
-5        int even = 0 ;
-6
-7        int k = 1 ;
-8        int a = 2 ;
-9        for( int i=1 ; i<=n ; i++ ){
-10            even+= k ;
-11            odd+= a ;
-12            k+=2 ;
-13            a+=2 ;
-14        }
-15        int ans = 1 ;
-16        for( int i = 1 ; i<= min(even,odd) ; i++ ){
-17            if( even%i==0 && odd%i==0 ){
-18                ans= i ;
-19            }
-20        }
-21        return ans ;
-22    }
-23};
+4        
+5        return n ;
+6    }
+7};
