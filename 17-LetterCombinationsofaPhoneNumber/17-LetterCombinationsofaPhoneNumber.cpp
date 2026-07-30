050c6118ba6d67391d37fe02bd5f15b1312bf7bb
Ram Lal <ramlal0801@gmail.com>
2026-07-30 21:33:13 +0530

    File updated at 30/7/2026, 9:33:15 pm

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.cpp
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.cpp
@@ -1,21 +1,27 @@
-// Last updated: 30/7/2026, 9:14:25 am
+// Last updated: 30/7/2026, 9:33:15 pm
 1class Solution {
 2public:
-3    int minimumPushes(string word) {
-4        int n = word.size() ;
-5        if( n<=8 ) return n ;
-6
-7        int ans = 8 ;
-8        
-9        int i = 2 ;
-10        n=n-8 ;
-11        while( (n/8)>0 ){
-12            ans += 8*i ;
-13            i++ ;
-14            n=n-8;
+3    vector<string> letterCombinations(string digits) {
+4        vector<string> res ;
+5        vector<string> map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+6        string curr = "";
+7        helper( 0 , digits , curr , res , map ) ;
+8        return res ;
+9    }
+10
+11    void helper(int ind , string &digits ,string &curr , vector<string> &res ,vector<string> &map ){
+12        if( ind==digits.length() ){
+13            res.push_back(curr);
+14            return ;
 15        }
-16        ans+=i*n ;
-17        return ans ;
+16
+17        string letters = map[digits[ind]-'0'] ;
 18
-19    }
-20};
+19        for( char c : letters ){
+20            curr+=c ;
+21            helper( ind+1 , digits , curr , res , map ) ;
+22            curr.pop_back();
+23        }
+24
+25    }
+26};
