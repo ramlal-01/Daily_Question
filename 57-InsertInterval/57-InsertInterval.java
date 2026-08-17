5968a5943a3b45f6c3d1e3ac2ea8b9d402fe4f5a
Ram Lal <ramlal0801@gmail.com>
2026-08-17 23:25:15 +0530

    File updated at 17/8/2026, 11:25:18 pm

--- 57-InsertInterval/57-InsertInterval.java
+++ 57-InsertInterval/57-InsertInterval.java
@@ -1,27 +1,27 @@
-// Last updated: 6/11/2025, 5:50:41 am
-class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
- List<int[]> result = new ArrayList<>();
-        int i = 0;
-        int n = intervals.length;
-
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            result.add(intervals[i]);
-            i++;
-        }
-
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        result.add(newInterval);
-        
-        while (i < n) {
-            result.add(intervals[i]);
-            i++;
-        }
-
-        return result.toArray(new int[result.size()][]);
-    }
-}
+// Last updated: 17/8/2026, 11:25:18 pm
+1class Solution {
+2    public int[][] insert(int[][] intervals, int[] newInterval) {
+3 List<int[]> result = new ArrayList<>();
+4        int i = 0;
+5        int n = intervals.length;
+6
+7        while (i < n && intervals[i][1] < newInterval[0]) {
+8            result.add(intervals[i]);
+9            i++;
+10        }
+11
+12        while (i < n && intervals[i][0] <= newInterval[1]) {
+13            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+14            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+15            i++;
+16        }
+17        result.add(newInterval);
+18        
+19        while (i < n) {
+20            result.add(intervals[i]);
+21            i++;
+22        }
+23
+24        return result.toArray(new int[result.size()][]);
+25    }
+26}
