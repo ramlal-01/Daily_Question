5d4e9e0f7834ee880ceb90c0a6089822a12506de
Ram Lal <ramlal0801@gmail.com>
2026-09-07 10:54:50 +0530

    File updated at 7/9/2026, 10:54:50 am

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,15 +1,16 @@
-// Last updated: 19/8/2026, 10:16:10 pm
+// Last updated: 7/9/2026, 10:54:50 am
 1class Solution {
 2    public int climbStairs(int n) {
-3        if( n<=2 ) return n ;
-4
-5        int prev1 =1  , prev = 2 ;
-6        int curr = 1 ;
-7        for( int i =3 ; i<=n ; i++ ){
-8            curr = prev1 + prev ; 
-9            prev1= prev ;
-10            prev = curr ;
+3        int arr[] = new int[n] ;
+4        if( n<=2 ) return n ;
+5
+6        arr[0] = 1 ;
+7        arr[1] = 2 ;
+8
+9        for( int i = 2 ; i<n ; i++ ){
+10            arr[i] = arr[i-1] + arr[i-2] ;
 11        }
-12        return curr ;
-13    }
-14}
+12
+13        return arr[n-1] ; 
+14    }
+15}
