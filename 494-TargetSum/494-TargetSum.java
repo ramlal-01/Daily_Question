99ce0ba8395586849fbff3feb306874e6c7caade
Ram Lal <ramlal0801@gmail.com>
2026-09-08 20:47:51 +0530

    File updated at 8/9/2026, 8:47:51 pm

--- 494-TargetSum/494-TargetSum.java
+++ 494-TargetSum/494-TargetSum.java
@@ -1,31 +1,28 @@
-// Last updated: 8/9/2026, 2:29:49 pm
+// Last updated: 8/9/2026, 8:47:51 pm
 1class Solution {
-2    public int findTargetSumWays(int[] nums, int target) {
-3        int n = nums.length ; 
+2    public int lastStoneWeightII(int[] stones) {
+3        int n = stones.length ;
 4        int sum = 0 ;
-5        for( int ele : nums ) sum+=ele ;
-6
-7        if ((sum + target) % 2 != 0) {
-8            return 0;
-9        }
-10        if( Math.abs(target)>sum ) return 0 ;
+5        for( int ele : stones ) sum+=ele ;
+6        int tot = sum ;
+7        sum=sum/2 ;
+8        boolean dp[][] = new boolean[n+1][sum+1] ;
+9
+10        for( int i = 0 ; i<=n ; i++ ) dp[i][0] = true ;
 11
-12        sum = ( sum+target)/2 ;
-13
-14        int dp[][] = new int[n+1][sum+1] ;
+12        for( int i = 1 ; i<=n ; i++ ){
+13            for( int j = 1 ; j<=sum ; j++ ){
+14                dp[i][j] = dp[i-1][j] ;
 15
-16        dp[0][0] = 1 ;
-17
-18        for( int i = 1 ; i<=n ; i++ ){
-19            for( int j = 0; j<=sum ; j++ ){
-20                dp[i][j] = dp[i-1][j] ;
+16                if( stones[i-1] <= j ){
+17                    dp[i][j] = dp[i][j] || dp[i-1][ j-stones[i-1] ] ;
+18                }
+19            }
+20        }
 21
-22                if( nums[i-1] <= j ){
-23                    dp[i][j] = dp[i][j] + dp[i-1][j-nums[i-1]] ;
-24                }
-25            }
-26        }
-27
-28        return dp[n][sum] ;
-29    }
-30}
+22        for( int i = sum ; i>=0 ; i-- ){
+23            if( dp[n][i] ) return tot - 2*i ;
+24        }
+25        return 0 ;
+26    }
+27}
