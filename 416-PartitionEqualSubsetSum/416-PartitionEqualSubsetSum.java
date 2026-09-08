8e22ee6a78c31dfa54c2144bfaca1a48d2f6da1b
Ram Lal <ramlal0801@gmail.com>
2026-09-08 13:48:19 +0530

    File updated at 8/9/2026, 1:48:19 pm

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,34 +1,28 @@
-// Last updated: 24/3/2026, 12:19:56 pm
+// Last updated: 8/9/2026, 1:48:19 pm
 1class Solution {
 2    public boolean canPartition(int[] nums) {
-3        int sum = 0;
-4        for( int ele : nums){
-5            sum+= ele;
-6        }
-7        if( sum%2!=0 ) return false;
-8        return helper( nums.length , sum/2 , nums);
-9    }
+3        int sum = 0 ;
+4        int n = nums.length ;
+5        for( int ele : nums ) sum+=ele ;
+6
+7        if( sum%2!=0 ) return false ;
+8
+9        sum = sum/2 ; 
 10
-11    public boolean helper( int n , int k , int[] nums){
-12        boolean dp[][] = new boolean[n+1][k+1] ;
-13        
-14        for( int i =0 ; i<n+1 ; i++ ){
-15            dp[i][0] = true ;
-16        }
-17        for( int i =1 ; i<k+1 ; i++ ){
-18            dp[0][i] = false ;
-19        }
-20        
-21        for( int i =1 ; i<=n ; i++ ){
-22            for( int j = 1 ; j<=k ; j++ ){
-23                 
-24                if( nums[i-1] <=j  ){
-25                    dp[i][j] =  dp[i-1][j-nums[i-1]] ||  dp[i-1][j] ;
-26                }
-27                else dp[i][j] = dp[i-1][j] ;
-28            }
-29        }
-30        
-31        return dp[n][k] ;
-32    }
-33}
+11        Boolean dp[][] = new Boolean[n+1][sum+1] ;
+12
+13        for( int i = 0 ; i<=n ; i++ ) dp[i][0] = true ;
+14        for ( int j = 1 ; j<=sum ; j++ ) dp[0][j] = false ;
+15
+16        for( int i = 1 ; i<=n ; i++ ){
+17            for( int j = 1; j<=sum ; j++ ){
+18                dp[i][j] = dp[i-1][j] ;
+19
+20                if( nums[i-1] <= j ){
+21                    dp[i][j] = dp[i][j] || dp[i-1][j-nums[i-1]] ;
+22                }
+23            }
+24        }
+25        return dp[n][sum] ;
+26    }
+27}
