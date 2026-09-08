ea126fefa2442622a033a6deb1e6abebd9820aef
Ram Lal <ramlal0801@gmail.com>
2026-09-09 02:52:17 +0530

    File updated at 9/9/2026, 2:52:16 am

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,19 +1,21 @@
-// Last updated: 26/3/2026, 10:53:43 am
+// Last updated: 9/9/2026, 2:52:16 am
 1class Solution {
 2    public int change(int amount, int[] coins) {
-3        int n = coins.length ;
-4        int dp[][] = new int[n+1][amount+1] ;
-5
-6        dp[0][0] = 1 ;
-7
-8        for( int i = 1 ; i<=n ; i++ ){
-9            for( int j = 0 ; j<=amount ; j++ ){
-10                if( coins[i-1]<=j ){
-11                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j] ;
-12                }
-13                else dp[i][j] = dp[i-1][j] ;
-14            }
-15        }
-16        return dp[n][amount] ;
-17    }
-18}
+3        int n = coins.length ; 
+4
+5        int dp[][] = new int[n+1][amount+1] ;
+6
+7        for( int i = 1; i<=n ; i++ ) dp[i][0] = 1 ;
+8
+9        for( int i = 1 ; i<=n ; i++ ){
+10            for( int j = 1; j<=amount ; j++ ){
+11                dp[i][j] = dp[i-1][j] ;
+12
+13                if( coins[i-1]<= j ){
+14                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+15                }
+16            }
+17        }
+18        return dp[n][amount] ;
+19    }
+20}
