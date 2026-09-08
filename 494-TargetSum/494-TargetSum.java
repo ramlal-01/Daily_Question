1c8559c64f7592e6e8d5c1aa360ca441ba3de442
Ram Lal <ramlal0801@gmail.com>
2026-09-08 14:29:49 +0530

    File updated at 8/9/2026, 2:29:49 pm

--- 494-TargetSum/494-TargetSum.java
+++ 494-TargetSum/494-TargetSum.java
@@ -1,32 +1,31 @@
-// Last updated: 8/9/2026, 2:21:47 pm
+// Last updated: 8/9/2026, 2:29:49 pm
 1class Solution {
 2    public int findTargetSumWays(int[] nums, int target) {
-3        int total = 0 ;
-4
-5        for( int ele : nums){
-6            total += ele;
-7        }
-8
-9        int s1  = target+total;
-10        if( s1%2!=0 || s1<0) return 0 ;
+3        int n = nums.length ; 
+4        int sum = 0 ;
+5        for( int ele : nums ) sum+=ele ;
+6
+7        if ((sum + target) % 2 != 0) {
+8            return 0;
+9        }
+10        if( Math.abs(target)>sum ) return 0 ;
 11
-12        s1 = ( target + total )/2 ;
+12        sum = ( sum+target)/2 ;
 13
-14        return subsetsum( nums , s1 );
+14        int dp[][] = new int[n+1][sum+1] ;
 15
-16    }
+16        dp[0][0] = 1 ;
 17
-18    public int subsetsum( int[] nums , int target){
-19        int dp[] = new int[target+1];
-20
-21        dp[0] = 1 ;
-22
-23        for( int i = 0 ; i<nums.length ; i++){
-24            for( int j = target ; j>=nums[i] ; j-- ){
-25                dp[j] = dp[j] + dp[ j-nums[i]];
-26            }
-27        }
-28
-29        return dp[target];
-30    }
-31}
+18        for( int i = 1 ; i<=n ; i++ ){
+19            for( int j = 0; j<=sum ; j++ ){
+20                dp[i][j] = dp[i-1][j] ;
+21
+22                if( nums[i-1] <= j ){
+23                    dp[i][j] = dp[i][j] + dp[i-1][j-nums[i-1]] ;
+24                }
+25            }
+26        }
+27
+28        return dp[n][sum] ;
+29    }
+30}
