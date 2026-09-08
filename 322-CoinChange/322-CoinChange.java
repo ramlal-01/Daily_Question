bcf1a25b910bdf0d053c21adf1498e9bef857106
Ram Lal <ramlal0801@gmail.com>
2026-09-09 02:39:38 +0530

    File updated at 9/9/2026, 2:39:38 am

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,27 +1,21 @@
-// Last updated: 26/3/2026, 11:37:06 am
+// Last updated: 9/9/2026, 2:39:38 am
 1class Solution {
-2    public int coinChange(int[] nums, int sum) {
-3        int n = nums.length;
-4        int INF = (int)1e9;
-5
-6        int[][] dp = new int[n + 1][sum + 1];
-7
-8        for (int j = 1; j <= sum; j++) {
-9            dp[0][j] = INF;
-10        }
-11
-12        for (int i = 1; i <= n; i++) {
-13            for (int j = 0; j <= sum; j++) {
-14
-15                dp[i][j] = dp[i - 1][j];
-16
-17                if (nums[i - 1] <= j) {
-18                    dp[i][j] = Math.min(dp[i-1][j],
-19                        1 + dp[i][j - nums[i - 1]]);
-20                }
-21            }
-22        }
-23
-24        return dp[n][sum] >= INF ? -1 : dp[n][sum];
-25    }
-26}
+2    public int coinChange(int[] coins, int amount) {
+3        int n = coins.length ; 
+4
+5        int dp[][] = new int[n+1][amount+1] ;
+6
+7        for( int i = 1; i<=amount ; i++ ) dp[0][i] = Integer.MAX_VALUE-1 ;
+8
+9        for( int i = 1 ; i<=n ; i++ ){
+10            for( int j = 1; j<=amount ; j++ ){
+11                dp[i][j] = dp[i-1][j] ;
+12
+13                if( coins[i-1]<= j ){
+14                    dp[i][j] = Math.min( dp[i-1][j] , 1 + dp[i][j-coins[i-1]]);
+15                }
+16            }
+17        }
+18        return dp[n][amount]==Integer.MAX_VALUE-1 ? -1 : dp[n][amount] ;
+19    }
+20}
