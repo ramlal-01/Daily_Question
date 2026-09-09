be71e97fc4ac326d91c5b02877ea7d90ec1b9b91
Ram Lal <ramlal0801@gmail.com>
2026-09-09 22:58:09 +0530

    File updated at 9/9/2026, 10:58:09 pm

--- 139-WordBreak/139-WordBreak.java
+++ 139-WordBreak/139-WordBreak.java
@@ -1,30 +1,17 @@
-// Last updated: 27/3/2026, 11:02:13 pm
+// Last updated: 9/9/2026, 10:58:09 pm
 1class Solution {
-2    int count  = 0 ;
-3    public int countArrangement(int n) { 
-4        boolean arr[] = new boolean[n+1];
-5        helper( n , arr , 1);
-6        
-7        // System.out.println(res);
-8        return count;
-9    }
-10
-11    public void helper( int n  , boolean[] arr , int ind ){
-12        if(  ind>n){
-13            count++;
-14            return ;
-15        }
-16        
-17
-18        for( int i = 1 ; i<=n ; i++){
-19            if( arr[i]) continue ;
-20
-21            arr[i] = true ; 
-22
-23            if( i% ind==0 || ind%i==0 ){ 
-24                helper( n , arr , ind+1); 
-25            }
-26            arr[i] = false;
-27        }
-28    }
-29}
+2    public boolean wordBreak(String s, List<String> wordDict) {
+3        int n = s.length();
+4        boolean dp[] = new boolean[n+1] ;
+5        dp[0] = true ;
+6        for (int i = 1; i <= n; i++) {
+7            for (int j = 0; j < i; j++) {
+8                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+9                    dp[i] = true;
+10                    break;
+11                }
+12            }
+13        }
+14        return dp[n];
+15    }
+16}
