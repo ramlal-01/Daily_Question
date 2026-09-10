5815d2ed8433a4c2b1c4a4756e14626be5dcdadd
Ram Lal <ramlal0801@gmail.com>
2026-09-10 11:40:34 +0530

    File updated at 10/9/2026, 11:40:35 am

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,20 +1,24 @@
-// Last updated: 4/11/2025, 2:55:34 pm
-class Solution { 
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length ; 
-        List<Integer> tails = new ArrayList<>();
-
-        for( int num : nums){
-            int i = Collections.binarySearch( tails , num);
-
-            if( i<0 ) i = -(i+1);
-
-            if( i== tails.size() ){
-                tails.add(num);
-            } else{
-                tails.set( i , num ); 
-            }
-        }
-        return tails.size();
-    }
-}
+// Last updated: 10/9/2026, 11:40:35 am
+1class Solution {
+2    public int lengthOfLIS(int[] nums) {
+3        int n = nums.length ;
+4        int dp[] = new int[n] ;
+5
+6        Arrays.fill(dp,1) ;
+7
+8        for( int i = 0 ; i<n ; i++ ){
+9            for( int j = 0 ; j<i ; j++ ){
+10                if( nums[j]<nums[i] ){
+11                    dp[i] = Math.max( dp[i] , dp[j] + 1 );
+12                    
+13                }
+14            }
+15        }
+16        int max = Integer.MIN_VALUE ;
+17        for( int i = 0 ; i<n ; i++ ){   
+18            max = Math.max( dp[i] , max ) ;
+19        }
+20
+21        return max ;
+22    }
+23}
