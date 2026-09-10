a866ec839c6833a45e25e56f4a354ec9e26da6d5
Ram Lal <ramlal0801@gmail.com>
2026-09-10 14:00:42 +0530

    File updated at 10/9/2026, 2:00:42 pm

--- 1262-GreatestSumDivisiblebyThree/1262-GreatestSumDivisiblebyThree.java
+++ 1262-GreatestSumDivisiblebyThree/1262-GreatestSumDivisiblebyThree.java
@@ -1,34 +1,28 @@
-// Last updated: 10/9/2026, 1:38:27 pm
+// Last updated: 10/9/2026, 2:00:42 pm
 1class Solution {
-2    public int maxSumDivThree(int[] nums) {
-3        int n = nums.length;
-4        int[][] dp = new int[n + 1][3];
-5
-6        // Initialize
-7        dp[0][0] = 0;
-8        dp[0][1] = Integer.MIN_VALUE;
-9        dp[0][2] = Integer.MIN_VALUE;
-10
-11        for (int i = 1; i <= n; i++) {
-12            int x = nums[i - 1];
-13            int mod = x % 3;
-14
-15            for (int r = 0; r < 3; r++) {
-16                // Case 1: Do NOT take x
-17                int notTake = dp[i - 1][r];
+2    Integer dp[][] ;
+3
+4    public int maxSumDivThree(int[] nums) {
+5        int n = nums.length ;
+6        dp = new Integer[n][3] ;
+7
+8        return helper( n-1 , nums , 0 ) ;
+9        
+10    }
+11
+12    public int helper( int ind  , int[] nums , int rem ){
+13        if( ind<0 ){
+14            return rem==0 ? 0 : Integer.MIN_VALUE ;
+15        }
+16
+17        if( dp[ind][rem]!=null ) return dp[ind][rem] ;
 18
-19                // Case 2: Take x
-20                int prevR = (r - mod + 3) % 3;
-21                int take = dp[i - 1][prevR];
-22                if (take != Integer.MIN_VALUE) {
-23                    take += x;
-24                }
-25
-26                dp[i][r] = Math.max(notTake, take);
-27            }
-28        }
-29
-30        return dp[n][0];
-31    }
-32}
-33
+19        int newRem = ( nums[ind]+rem )%3  ; 
+20        int take = helper( ind-1 , nums , newRem )  ;
+21
+22        take+= nums[ind] ;
+23        int skip = helper( ind-1 , nums , rem ) ;
+24
+25        return dp[ind][rem] = Math.max(take,skip);
+26    }
+27}
